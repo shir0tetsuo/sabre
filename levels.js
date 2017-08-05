f3e59acf135d50b9f3fa1a446f9394218451e50c
root <root@dnet.lab>
2017-08-05 19:16:49 -0400

    Multi-Level JSON String Test (Success)

--- levels.js
+++ levels.js
@@ -5,3 +5,9 @@ console.log(levl.level2)
 console.log(levl.level3)
 console.log(levl.level4.level4)
 console.log(levl)
+// Test: External text variables in math functions
+const game = require("./multilevel2.json")
+console.log(game)
+console.log(game.game)
+var gamemat = game.game[Math.floor(Math.random() * game.game.length)]
+console.log(gamemat.text)
