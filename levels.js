e59eff23c02d0269e31b4b53208d045e89b7112e
root <root@dnet.lab>
2017-08-05 18:53:05 -0400

    Multi-Leveled JSON Test

---
+++ levels.js
@@ -0,0 +1,7 @@
+// Testing levels in json
+const levl = require("./multilevel.json")
+console.log(levl.level1)
+console.log(levl.level2)
+console.log(levl.level3)
+console.log(levl.level4.level4)
+console.log(levl)
