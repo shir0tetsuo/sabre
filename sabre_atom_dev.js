c6a69fd0d47edeef9aed28045ff0346908bbdbab
shir0tetsuo <shadowsword@protonmail.com>
2017-08-09 15:29:58 -0400

    Digging deeper

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -85,10 +85,10 @@ client.on("message", (message) => {
             regex = xksregex[1]
             if (stdout === "true") {
               var regval = ":large_orange_diamond: String was FOUND!"
-            } else {
+            } else if (stdout === "false") {
               var regval = ":large_blue_diamond: String was NOT FOUND!"
             }
-            console.log(regval, regex)
+            console.log(regval, regex, stdout, stderr)
             const embed = new Discord.RichEmbed()
               .setTitle('XKeyScore Regex')
               .setAuthor('firewall.dnet.lab', 'https://www.google.ca/search?q=xkeyscore')
