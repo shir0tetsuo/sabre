8073adf4e611ae2dd9397759b03bf657758b26f7
shir0tetsuo <shadowsword@protonmail.com>
2017-08-09 15:34:33 -0400

    Test

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -82,13 +82,13 @@ client.on("message", (message) => {
       if (xksregex[1] !== undefined) {
         exec('/root/NC/utils/NorthStar/xks.sh ' + xksregex[1],
           function(error, stdout, stderr) {
-            var regex = xksregex[1]
+            let regex = xksregex[1]
             if (stdout === "true") {
               var regval = ":large_orange_diamond: String was FOUND!"
             } else if (stdout === "false") {
               var regval = ":large_blue_diamond: String was NOT FOUND!"
             }
-            console.log(regval)
+            //console.log(regval)
             console.log(regex)
             console.log(stdout)
             console.log(stderr)
