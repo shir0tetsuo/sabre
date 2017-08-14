54519b398d071f81bcdd021353789e4e53e5871a
shir0tetsuo <shadowsword@protonmail.com>
2017-08-14 19:13:50 -0400

    ge load test

--- ge.js
+++ ge.js
@@ -1,3 +1,23 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Project Gamma Emerald - Security bot for DAVNET
 ////////////////////////////////////////////////////////////////////////////////
+// Codename David
+//    message.guild.channels.find("id", config.chan.securitybot).send("Hello, World!")
+let syslog = "Gamma Emerald: "
+console.log(syslog, "System Initialization.")
+// client -> ge
+const Discord = require("discord.js");
+const ge = new Discord.Client();
+console.log(syslog, "Discord Loaded to Memory.");
+// Constraints
+const key = require("./token.json");
+const cfg = require("./ge_init");
+let prefix = cfg.pre;
+console.log(syslog, "TOKEN: ", key.token_ge);
+console.log(syslog, "SOFTWAREVERSION: ", cfg.version);
+console.log(syslog, "PREFIX: ", prefix);
+console.log(syslog, "ADMINISTRATOR: ", cfg.id.administrator);
+console.log(syslog, "OPER: ", cfg.guild_davnet_securitybot);
+console.log(syslog, "Beginning Authentication Process.");
+
+console.log(syslog, "System Ready.")
