77e5ac8f4d3e69754bee5a1422a674cb25c06d02
shir0tetsuo <shadowsword@protonmail.com>
2017-08-14 20:22:15 -0400

    Need blank killnum to work

--- xge.s
+++ xge.s
@@ -1,6 +1,6 @@
 #!/bin/bash
 killnum=$(ps awuxx | grep "node ge.js" | grep -v "grep" | awk {'print $2'} | head -n 1)
-if [ -z killnum ]; then
+if [[ -z killnum ]]; then
 echo -e "\e[1;31m Process Not Found\e[m"
 else
 kill -9 $killnum;
