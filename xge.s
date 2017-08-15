1a7041025cf1a010f375029ae67fd74d35be45d2
shir0tetsuo <shadowsword@protonmail.com>
2017-08-14 20:17:42 -0400

    Modified kill script

--- xge.s
+++ xge.s
@@ -1,5 +1,9 @@
 #!/bin/bash
 killnum=$(ps awuxx | grep "node ge.js" | grep -v "grep" | awk {'print $2'} | head -n 1)
-if [ -z killnum ]; then kill -9 $killnum; fi
+if [ -z killnum ]; then
+echo -e "\e[1;31m Process Not Found\e[m"
+else
+kill -9 $killnum;
+fi
 echo -e "\e[1;31mProcess $killnum terminated.\e[m"
 node ge.js & disown
