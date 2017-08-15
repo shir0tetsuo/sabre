07089ac163f8ce3dae86a059d8267b26fb2b37b9
shir0tetsuo <shadowsword@protonmail.com>
2017-08-14 20:11:59 -0400

    Modified echo

--- xge.s
+++ xge.s
@@ -1,5 +1,5 @@
 #!/bin/bash
 killnum=$(ps awuxx | grep "node ge.js" | awk {'print $2'})
 if [ -z killnum ]; then kill $killnum; fi
-echo -e "\e[1;31mProcess \e[m"
+echo -e "\e[1;31mProcess $killnum terminated.\e[m"
 node ge.js & disown
