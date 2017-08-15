4aa014261f11ade8ba1c90094ac7088a625e5cda
shir0tetsuo <shadowsword@protonmail.com>
2017-08-15 01:43:13 -0400

    Changed disown sequence

--- botrun.s
+++ botrun.s
@@ -2,7 +2,7 @@
 killall node
 sleep 2s
 ./pull.sh
-su -c "node sabre_atom_dev.js & disown" -s /bin/sh octagon
+su -c "node sabre_atom_dev.js" -s /bin/sh octagon & disown
 #node ge.js & disown
 echo -e "\e[1;31mBoot Complete.\e[m"
 #node sabre_bot.js
