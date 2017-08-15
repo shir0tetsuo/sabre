c931e0c38ddc14f05e8cfe59e98936ccb96a3758
shir0tetsuo <shadowsword@protonmail.com>
2017-08-15 01:42:14 -0400

    De-Escalated permissions from root

--- botrun.s
+++ botrun.s
@@ -2,7 +2,7 @@
 killall node
 sleep 2s
 ./pull.sh
-node sabre_atom_dev.js & disown
+su -c "node sabre_atom_dev.js & disown" -s /bin/sh octagon
 #node ge.js & disown
 echo -e "\e[1;31mBoot Complete.\e[m"
 #node sabre_bot.js
