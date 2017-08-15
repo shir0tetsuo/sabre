a60c21ed54327964afb6d078bfc18a31d978f96e
shir0tetsuo <shadowsword@protonmail.com>
2017-08-14 20:25:47 -0400

    Moved echo around

--- xge.s
+++ xge.s
@@ -1,9 +1,9 @@
 #!/bin/bash
 killnum=$(ps awuxx | grep "node ge.js" | grep -v "grep" | awk {'print $2'} | head -n 1)
 if [[ -z $killnum ]]; then
-echo -e "\e[1;31m Process Not Found\e[m"
+echo -e "\e[1;31mProcess Not Found\e[m"
 else
 kill -9 $killnum;
-fi
 echo -e "\e[1;31mProcess $killnum terminated.\e[m"
+fi
 node ge.js & disown
