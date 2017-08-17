5906fbf22be58198b8e0304e24e19738c60b6fb3
shir0tetsuo <shadowsword@protonmail.com>
2017-08-17 00:42:41 -0400

    Test Update

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -99,7 +99,7 @@ function readLevel(mess) {
         text: client.user.username
       },
       author: {
-        name: mess.author.username,
+        name: mess.guild.member.nickname,
         icon_url: mess.author.avatarURL
       },
       fields: [
