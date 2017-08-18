c0d7d875ceb3c89826f9a1d5db7b90a6fc9387d6
shir0tetsuo <shadowsword@protonmail.com>
2017-08-18 19:59:38 -0400

    Fixed syntax error

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -598,7 +598,9 @@ client.on("message", (message) => {
       return;
     } else if (devarg === "poke" && message.author.id === config.perUser.ownerID) {
       message.reply("``Developer Command was Run. This command should not be used again.``")
-      sql.run("CREATE TABLE IF NOT EXISTS makeitrain (tickets INTEGER) VALUES (?)", [100]);
+      sql.run("CREATE TABLE IF NOT EXISTS makeitrain (tickets INTEGER)").then(() => {
+        sql.run("INSERT INTO makeitrain (tickets) VALUES (?)", [100]);
+      })
     } else if (devarg === "inspect" && message.member.roles.has(config.role.alaska_botdev)) {
       if (message.mentions.members.first() === undefined) return;
       sql.get(`SELECT * FROM scores WHERE userId ="${message.mentions.members.first().id}"`).then(row => {
