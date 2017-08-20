a1191a631c496eaa92e2e113c897193e514687d5
shir0tetsuo <shadowsword@protonmail.com>
2017-08-20 04:45:16 -0400

    changed log place

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -768,8 +768,8 @@ client.on("message", (message) => {
       if (devhandle[2] === undefined) return;
       scoreInit(message)
       message.reply("Developer Command was Run. Self-Seeded " + devhandle[2]*1 + curren)
-      console.log(chalk_dat(devhandle[2]*1))
       sql.run(`UPDATE scores SET tickets = "${devhandle[2]*1}" WHERE userId = "${message.author.id}"`)
+      console.log(chalk_dat(devhandle[2]*1))
     } else if (devarg === "selfseedL" && message.member.roles.has(config.role.alaska_specialdev)) {
       if (devhandle[2] === undefined) return;
       var seed = devhandle[2]*1
