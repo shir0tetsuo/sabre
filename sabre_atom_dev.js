7d972ba2f986ae6b339dca7bc74a0fc34113f2dd
shir0tetsuo <shadowsword@protonmail.com>
2017-08-20 00:26:29 -0400

    synt

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -196,7 +196,7 @@ function scoreDisplay(mess) {
 //uniq8
 function scanKeyword(mess) {
   const chanid = mess.channel
-  if mess.content.has(['AMBA', 'amba']).then(function (mess) {
+  if (mess.content.has(['AMBA', 'amba'])).then(function (mess) {
     mess.delete()
   })
   chanid.send("``CLASSIFIED``")
