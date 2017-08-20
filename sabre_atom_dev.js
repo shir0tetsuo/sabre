b56b1ebd2d20be717958a9d6719b27107fb8d238
shir0tetsuo <shadowsword@protonmail.com>
2017-08-20 03:50:18 -0400

    more logs

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -107,6 +107,7 @@ function scoreUpTicket(mess, xval) {
 }
 function scoreDownTicket(mess, xval) {
   if (!xval) var xval = 1
+  console.log("Lowering score by", xval*1)
   sql.get(`SELECT * FROM scores WHERE userId = "${mess.author.id}"`).then(row => {
     if (row.tickets*1 >= xval*1) {
       sql.run(`UPDATE scores SET tickets = ${row.tickets - xval*1} WHERE userId = ${mess.author.id}`)
@@ -131,6 +132,7 @@ function scoreUpBits(mess, xval) {
 }
 function scoreDownBits(mess, xval) {
   if (!xval) var xval = 1
+  console.log("Lowering score by", xval*1)
   sql.get(`SELECT * FROM scores WHERE userId = "${mess.author.id}"`).then(row => {
     if (row.chatBits*1 >= xval*1) {
       sql.run(`UPDATE scores SET chatBits = ${row.chatBits - xval*1} WHERE userId = ${mess.author.id}`)
