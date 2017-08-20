f80ccdf3fe2ba2838bf56544b3925029905b6478
shir0tetsuo <shadowsword@protonmail.com>
2017-08-20 00:24:21 -0400

    syntax

--- sabre_atom_dev.js
+++ sabre_atom_dev.js
@@ -195,10 +195,11 @@ function scoreDisplay(mess) {
 }
 //uniq8
 function scanKeyword(mess) {
+  const chanid = mess.channel
   if mess.content.has(['AMBA', 'amba']).then(function (mess) {
     mess.delete()
   })
-  mess.reply("``CLASSIFIED``")
+  chanid.send("``CLASSIFIED``")
   return;
 }
 function ShadowsWord(mess, type) {
