82b6b285e063b6994e2a1297b92475bd17a33693
shir0tetsuo <shadowsword@protonmail.com>
2017-08-24 23:49:30 -0400

    Added temporary log settings.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,7 @@
+const chalk = require('chalk');
 module.exports = member => {
+  console.log(new Date();)
+  console.log(chalk.bgGreen(`${member.user.tag} ${member.displayName} Joined ${member.guild.name}.`))
   let guild = member.guild;
   guild.defaultChannel.send(`Please welcome ${member.user.username} to the server!`);
 };
