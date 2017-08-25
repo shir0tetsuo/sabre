1fb830a759e72ea61a9ccd4fe9d50e6b0a19b729
shir0tetsuo <shadowsword@protonmail.com>
2017-08-25 18:59:46 -0400

    Heavily modified Remove to match Add

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,7 +1,35 @@
+const settings = require('../settings.json')
 const chalk = require('chalk');
 module.exports = member => {
   console.log(new Date());
   console.log(chalk.bgYellow.black(`${member.user.tag} ${member.displayName} Left ${member.guild.name}.`))
   let guild = member.guild;
-  guild.defaultChannel.send(`Please say goodbye to ${member.user.username} we will miss you!`);
+  if (guild.id === settings.davnetguild) {
+    //let davnet_gmA = guild.channels.find('name', 'security-bot')
+    guild.channels.find('name', 'security-bot').send({embed: {
+      color: 0xA7A7A5,
+      timestamp: new Date(),
+      fields: [
+        { //member.user.username
+          name: member.user.tag + " (" + member.displayName + ")",
+          value: "Parted the server."
+        }
+      ]
+    }})
+  } else if (guild.id === settings.alaskaguild) {
+    //let alaska = guild.channels.get(settings.alaskachanclassified)
+    //let alaska_gmA = guild.channels.find('name', 'classified')
+    guild.channels.find('name', 'classified').send({embed: {
+      color: 0xA7A7A5,
+      timestamp: new Date(),
+      fields: [
+        { //member.user.username
+          name: member.user.tag + " (" + member.displayName + ")",
+          value: "Parted the server."
+        }
+      ]
+    }})
+  } else {
+    guild.defaultChannel.send(`Please say goodbye to ${member.user.username} we will miss you!`);
+  }
 };
