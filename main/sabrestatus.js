eec642172ea938ac6ef688578d4d856fafa07d8a
shir0tetsuo <shadowsword@protonmail.com>
2017-08-24 23:14:13 -0400

    Sabre Status

---
+++ main/sabrestatus.js
@@ -0,0 +1,40 @@
+const settings = require('../settings.json');
+exports.run = (client, message, params) => {
+  if (!params[0]) return;
+  const detail = message.content.split(/\s+/g);
+  if (detail[1] === "online" || detail[1] === "idle" || detail[1] === "dnd" || detail[1] === "offline" || detail[1] === "invisible") {
+    client.user.setStatus(`${detail[1]}`)
+    message.reply("`Access Granted` Sabre's Status was Updated.")
+    console.log(new Date())
+    console.log(chalk.greenBright(message.member.displayName), chalk.yellow(message.content), chalk.blueBright(message.guild.name, message.channel.name));
+    console.log(chalk.yellow("Sabre's Status changed."))
+  } else if (detail[1] === "game") {
+    client.user.setPresence({ game: { name: `${message.content.substring(18,128)}`, type: 0}})
+    message.reply("`Access Granted` Sabre's Presence was Updated.")
+    console.log(new Date())
+    console.log(chalk.greenBright(message.member.displayName), chalk.yellow(message.content), chalk.blueBright(message.guild.name, message.channel.name));
+    console.log(chalk.yellow("Sabre's Game Status changed."))
+  } else if (detail[1] === "reset") {
+    client.user.setPresence({ game: { name: `With ${client.guilds.size} Servers. v${settings.version}`, type: 0}})
+    client.user.setStatus("dnd")
+    message.reply("`Access Granted` Sabre's Presence was reset.")
+    console.log(new Date())
+    console.log(chalk.greenBright(message.member.displayName), chalk.yellow(message.content), chalk.blueBright(message.guild.name, message.channel.name));
+    console.log(chalk.yellow("Sabre's Status was reset."))
+  } else {
+    message.reply("`Access Denied` The argument was not understood.")
+  };
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['saberstatus'],
+  permLevel: 3
+};
+
+exports.help = {
+  name: 'sabrestatus',
+  description: 'Changes Sabre\'s Status. Permission Level 3.',
+  usage: 'sabrestatus [game/online/reset/dnd/idle/invisible/offline] [game-message]'
+};
