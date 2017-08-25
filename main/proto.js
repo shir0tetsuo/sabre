16b96249c364c8a734dacb9c789a20a44f6ef79f
shir0tetsuo <shadowsword@protonmail.com>
2017-08-24 20:52:46 -0400

    Prototype command added.

---
+++ main/proto.js
@@ -0,0 +1,19 @@
+const settings = require('../settings.json');
+exports.run = (client, message, params) => {
+  if (!params[0]) return;
+  console.log(chalk.yellow(message.content));
+  message.reply("`Access Granted` System logged snowflake for object.");
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['prototype'],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'prototype',
+  description: 'Displays the mentioned object as a snowflake.',
+  usage: 'prototype [mention]'
+};
