62e23d302e70011613d7bf6d3805c5cbcc1f544f
shir0tetsuo <shadowsword@protonmail.com>
2017-08-24 20:55:10 -0400

    Attempting to resolve syntax error.

--- main/help.js
+++ main/help.js
@@ -1,4 +1,5 @@
 const settings = require('../settings.json');
+const chalk = require ('chalk');
 exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
