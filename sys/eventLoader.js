a7bd7a2496308c6295ff575ecb4d46b17678a75f
shir0tetsuo <shadowsword@protonmail.com>
2017-08-24 21:39:52 -0400

    Removed updating

--- sys/eventLoader.js
+++ sys/eventLoader.js
@@ -6,7 +6,7 @@ module.exports = client => {
   client.on('message', reqEvent('message'));
   client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
   client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
-  client.on('guildMemberUpdate', reqEvent('guildMemberUpdate'));
+//  client.on('guildMemberUpdate', reqEvent('guildMemberUpdate'));
   client.on('guildBanAdd', reqEvent('guildBanAdd'));
   client.on('guildBanRemove', reqEvent('guildBanRemove'));
 };
