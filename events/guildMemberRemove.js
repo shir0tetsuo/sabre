0a267aca3bb499df8b25fcd355b79e5c35268dcf
shir0tetsuo <shadowsword@protonmail.com>
2017-08-24 23:50:24 -0400

    Syntax fix

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 module.exports = member => {
-  console.log(new Date();)
+  console.log(new Date());
   console.log(chalk.bgYellow(`${member.user.tag} ${member.displayName} Left ${member.guild.name}.`))
   let guild = member.guild;
   guild.defaultChannel.send(`Please say goodbye to ${member.user.username} we will miss you!`);
