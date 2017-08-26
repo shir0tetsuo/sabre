2afba1a24db66665cdcdc482c3f5483453f7cf5f
shir0tetsuo <shadowsword@protonmail.com>
2017-08-25 20:08:30 -0400

    Heavily modified guildBanRemove

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -1,12 +1,32 @@
 const Discord = require('discord.js');
-
+require('../settings.json')
+const chalk = require('chalk');
 module.exports = (guild, user) => {
-
-  guild.defaultChannel.send(`${user.tag} was just unbanned!`);
-  const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setDescription(`**Action:** Unban\n**Target:** ${user.tag}\n**Moderator:** ${guild.client.unbanAuth.tag}\n**Reason:** ${guild.client.unbanReason}`);
-  return guild.channels.get(guild.channels.find('name', 'mod-log').id).send({embed});
-
+  console.log(new Date());
+  console.log(chalk.bgWhite.black(`${user.username} was UNBANNED from ${guild.name}.`))
+  if (guild.id === settings.davnetguild) {
+    guild.channels.find('name', 'security-bot').send({embed: {
+      color: 0xFFFF00,
+      timestamp: new Date(),
+      fields: [
+        { //member.user.username
+          name: user.tag,
+          value: "Was just unbanned!"
+        }
+      ]
+    }})
+  } else if (guild.id === settings.alaskaguild) {
+    guild.channels.find('name', 'classified').send({embed: {
+      color: 0xFFFF00,
+      timestamp: new Date(),
+      fields: [
+        { //member.user.username
+          name: user.tag,
+          value: "Was just unbanned!"
+        }
+      ]
+    }})
+  } else {
+    guild.defaultChannel.send(`${user.tag} was just unbanned!`);
+  }
 };
