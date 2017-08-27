ea36c6900da817ead0ac7a17f038d6f0df1d50c5
shir0tetsuo <shadowsword@protonmail.com>
2017-08-27 12:27:00 -0400

    test

--- main/level.js
+++ main/level.js
@@ -40,11 +40,10 @@ function scoreDisplay(mess) {
           value: `System returned message in ${m.createdTimestamp - mess.createdTimestamp}ms.`
         }
       ]
-    }})).then(function (mess) {
-      if (mess.author.id === settings.ownerid) {
-        mess.reply("Is a Developer!")
-      }
-    })
+    }})
+  if (mess.author.id === settings.ownerid) {
+    mess.reply("Hello, World!")
+  })
   }).catch(function() {
     console.log(chalk.redBright("Bug at levels"))
   })
