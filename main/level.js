3fdc1a7646c305c03681f4f1e6d767234d40c382
shir0tetsuo <shadowsword@protonmail.com>
2017-08-27 15:30:09 -0400

    levels update

--- main/level.js
+++ main/level.js
@@ -7,6 +7,7 @@ let chatBit = ":eye_in_speech_bubble:"
 function scoreDisplay(mess) {
   sql.get(`SELECT * FROM scores WHERE userId = "${mess.author.id}"`).then(row => {
     if (!row) return mess.reply("Your current level is 0.");
+    console.log(mess.author.id, row.level, row.tickets, row.chatBits)
     mess.reply("Calculating!").then(m => m.edit({embed: {
       color: 0xFFC000,
       timestamp: new Date(),
