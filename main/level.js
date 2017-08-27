8ae3e2570893a757d1e3e200452e277f488f63ba
shir0tetsuo <shadowsword@protonmail.com>
2017-08-27 15:41:29 -0400

    moved score back one

--- main/level.js
+++ main/level.js
@@ -1,5 +1,5 @@
 const sql = require("sqlite");
-sql.open("./score.sqlite");
+sql.open("../score.sqlite");
 const settings = require('../settings.json');
 const chalk = require ('chalk');
 let curren = ":tickets:"
