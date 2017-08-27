c3f5040863da57a8d432781c5b2b7ff052d4f803
shir0tetsuo <shadowsword@protonmail.com>
2017-08-27 19:40:35 -0400

    File wants to stay nested a level above

--- main/level.js
+++ main/level.js
@@ -1,5 +1,5 @@
 const sql = require("sqlite");
-sql.open("/root/NC/utils/NorthStar/sabre.discord.js/score.sqlite");
+sql.open("../score.sqlite");
 const settings = require('../settings.json');
 const chalk = require ('chalk');
 let curren = ":tickets:"
