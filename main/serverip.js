a7b0b972c02e1130c43f23fc92e256c25386843d
shir0tetsuo <shadowsword@protonmail.com>
2017-08-30 01:07:06 -0400

    server IP command

---
+++ main/serverip.js
@@ -0,0 +1,25 @@
+const settings = require('../settings.json');
+var exec = require('child_process').exec;
+exports.run = (client, message, params) => {
+  if (message.author.id === settings.ownerid) {
+    exec('curl icanhazip.com',
+      function(error, stdout, stderr) {
+        message.author.send(stdout)
+      })
+  } else {
+    message.reply("`ERROR` You are not the Owner!")
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['sip', 'serverip'],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'serverIP',
+  description: 'Displays the Server IP. Owner Only.',
+  usage: 'sip'
+};
