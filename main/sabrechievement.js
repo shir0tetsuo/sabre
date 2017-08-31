aa4be153a95c92bbad3627f3f9be1ba017ec6df3
shir0tetsuo <shadowsword@protonmail.com>
2017-08-31 05:52:18 -0400

    modified slice

--- main/sabrechievement.js
+++ main/sabrechievement.js
@@ -4,26 +4,27 @@ exports.run = (client, message, params) => {
   if (params[0] === null || params[1] === null) return message.reply("`ERROR` See Manual!")
   if (params[0] === "green") {
     var slice = 20
-    var slCol = 0x00FF00
+    var slCol = 0x96E000
   } else if (params[0] === "red") {
     var slice = 18
-    var slCol = 0xFF0000
+    var slCol = 0xAB0000
   } else if (params[0] === "yellow") {
     var slice = 21
-    var slCol = 0xFFFF00
+    var slCol = 0xFFED48
   } else if (params[0] === "blue") {
     var slice = 19
-    var slCol = 0x0000FF
+    var slCol = 0x0049C0
   } else return message.reply("`ERROR` See Manual!")
   if (message.mentions.members.first() !== undefined) {
     let person = message.mentions.members.first()
     var authName = person.displayName
     var authIcon = client.user.avatarURL
+    var slice = slice + 23
   } else {
     var authName = message.member.displayName
     var authIcon = message.author.avatarURL
   }
-  let msg = message
+  let msg = message // ADD REACTS!
   message.delete()
   data = message.content.substring(slice, 1024)
   msg.channel.send({ embed: {
@@ -39,7 +40,7 @@ exports.run = (client, message, params) => {
     },
     fields: [
       {
-        name: `Achievement Unlocked!`,
+        name: `:large_orange_diamond: Achievement Unlocked!`,
         value: `${data}`
       }
     ]
