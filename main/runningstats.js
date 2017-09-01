c718653df47e77b89a04caf1a139f4034c49424e
shir0tetsuo <shadowsword@protonmail.com>
2017-08-31 20:48:05 -0400

    added This Server

--- main/runningstats.js
+++ main/runningstats.js
@@ -4,14 +4,14 @@ exports.run = (client, message, params) => {
   message.channel.send({ embed: {
     color: 0x009DC4,
     timestamp: new Date(),
-    description: `Sabre v${settings.version}`,
+    description: `v${settings.version}`,
     author: {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
     fields: [
       {
-        name: `${message.guild.name}`,
+        name: `This Server: ${message.guild.name}`,
         value: `:minidisc: Memory Usage: **${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB.**\n:tropical_fish: Users: **${client.users.size}**\n:satellite: Servers: **${client.guilds.size}**\n:biohazard: Channels: **${client.channels.size}**\n:gear: Node: ${process.version}`
       }
     ]
