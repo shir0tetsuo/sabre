4dcefa75b6803641505d373f0e3587b2f5c88f48
shir0tetsuo <shadowsword@protonmail.com>
2017-08-31 20:46:38 -0400

    c-u-av

--- main/runningstats.js
+++ main/runningstats.js
@@ -7,11 +7,11 @@ exports.run = (client, message, params) => {
     description: `Sabre v${settings.version}`,
     author: {
       name: client.user.username,
-      icon_url: message.author.avatarURL
+      icon_url: client.user.avatarURL
     },
     fields: [
       {
-        name: `\u200b`,
+        name: `${message.guild.name}`,
         value: `:minidisc: Memory Usage: **${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB.**\n:tropical_fish: Users: **${client.users.size}**\n:satellite: Servers: **${client.guilds.size}**\n:biohazard: Channels: **${client.channels.size}**\n:gear: Node: ${process.version}`
       }
     ]
