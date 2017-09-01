ce6495909e407f03ebbf9642fb443016c2c990ca
shir0tetsuo <shadowsword@protonmail.com>
2017-08-31 20:52:58 -0400

    made it run something

--- main/runningstats.js
+++ main/runningstats.js
@@ -1,21 +1,25 @@
 const settings = require('../settings.json');
 const Discord = require ("discord.js");
 exports.run = (client, message, params) => {
-  message.channel.send({ embed: {
-    color: 0x009DC4,
-    timestamp: new Date(),
-    description: `v${settings.version}`,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    fields: [
-      {
-        name: `This Server: ${message.guild.name}`,
-        value: `:minidisc: Memory Usage: **${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB.**\n:tropical_fish: Users: **${client.users.size}**\n:satellite: Servers: **${client.guilds.size}**\n:biohazard: Channels: **${client.channels.size}**\n:gear: Node: ${process.version}`
-      }
-    ]
-  }})
+  exec('uptime',
+    function(error, stdout, stderr) {
+      message.channel.send({ embed: {
+        color: 0x009DC4,
+        timestamp: new Date(),
+        description: `v${settings.version}`,
+        author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+        },
+        fields: [
+          {
+            name: `This Server: ${message.guild.name}`,
+            value: `:minidisc: Memory Usage: **${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**\n:tropical_fish: Users: **${client.users.size}**\n:satellite: Servers: **${client.guilds.size}**\n:biohazard: Channels: **${client.channels.size}**\n:gear: Node: ${process.version}\n:white_sun_small_cloud: Server Uptime: **${stdout}**`
+          }
+        ]
+      }})
+    }
+
 };
 
 exports.conf = {
