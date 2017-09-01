867b507f33d7f07255ffa65e3220eb2f828dfb99
shir0tetsuo <shadowsword@protonmail.com>
2017-09-01 16:52:07 -0400

    waterfall beta

---
+++ main/waterfall.js
@@ -0,0 +1,55 @@
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+exports.run = (client, message, params) => {
+  if (params[0] === undefined) return message.reply("`ERROR` Can't play option-less.")
+  if (params[0] === "usage" || params[0] === "help") {
+    message.channel.send({embed: {
+      color: 0x4D94E6,
+      timestamp: new Date(),
+      footer: {
+        text: "Waterfall Usage"
+      },
+      author: {
+        name: message.member.displayName,
+        icon_url: message.author.avatarURL
+      },
+      fields: [
+        {
+          name: "How the Game Works",
+          value: "A set of 5 cards are randomly generated. Your objective is to clear the game's 5 cards."
+        },
+        {
+          name :"Whatnow?",
+          value: "You can clear a card and move onto the next by guessing if the next card that will replace it will be higher or lower valued."
+        },
+        {
+          name: "What happens if I get it wrong?",
+          value: "You have to go back to the beginning and start over."
+        },
+        {
+          name: "What happens if the card is the same value?",
+          value: "You continue but are penalized."
+        },
+        {
+          name: "Commands?",
+          value: "See " + prefix + "help waterfall"
+        }
+      ]
+    }})
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['wf'],
+  permLevel: 1
+};
+
+exports.help = {
+  name: 'waterfall',
+  description: 'Waterfall Game. See wf usage. PermLVL 1.',
+  usage: 'wf [new/end/usage/view/high/low]'
+};
