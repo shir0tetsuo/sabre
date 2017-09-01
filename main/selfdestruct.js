73f47db4b9bdf4184c5524858a85ffeb90229469
shir0tetsuo <shadowsword@protonmail.com>
2017-08-31 22:36:03 -0400

    test 1

---
+++ main/selfdestruct.js
@@ -0,0 +1,34 @@
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+
+exports.run = (client, message, params) => {
+  if (params[1] === undefined) {
+    message.reply("Missing Content!")
+    message.delete()
+  }
+  if (params[0] < 1) {
+    message.reply("First argument must be a number!")
+    message.author.send("`ERROR: Number Required. Original Message: `" + message.content)
+    message.delete()
+  }
+  const cmdArg = message.content.split(/\s+/g)
+  let cLen = cmdArg[1].length + 1;
+  let pLen = params[0].length + 1;
+  let delaySec = params[0]
+  let delayMil = params[0]*1000
+  let cut = cLen + pLen
+  message.channel.send(message.content.substring(cut, 1024) + "``" + `Self Destruct in ${delaySec} Seconds.` + "``").delete(delayMil)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['del'],
+  permLevel: 1
+};
+
+exports.help = {
+  name: 'selfdestruct',
+  description: 'Creates a self-destructing message. PermLVL 1.',
+  usage: 'del <delay in seconds> [message]'
+};
