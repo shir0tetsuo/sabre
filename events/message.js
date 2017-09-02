de1b2a4566bdeb93b9e3fd04d60c88eb800336bf
shir0tetsuo <shadowsword@protonmail.com>
2017-09-01 20:10:35 -0400

    undef

--- events/message.js
+++ events/message.js
@@ -113,7 +113,7 @@ function scanKeyword(mess) {
 }
 
 function scanReply(message) {
-  if (message.mentions.members.first() === null) return;
+  if (message.mentions.members.first() === undefined) return;
   if (message.mentions.members.first().id === "339590992044752897") { // or client.id
     console.log ("Hello, World!")
   }
