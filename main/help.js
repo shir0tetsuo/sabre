9c4f5d3961151e41a3e6b7ae66a95c5cb49f48c5
shir0tetsuo <shadowsword@protonmail.com>
2017-09-02 05:18:34 -0400

    log hLen

--- main/help.js
+++ main/help.js
@@ -5,6 +5,7 @@ exports.run = (client, message, params) => {
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     let page = params[0]
     let hLen = 0 + (params[0]-1 * 1800)
+    console.log(hLen)
     message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n[help page ${page}]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n').substring(hLen,(hLen+1800))}`, {code:'asciidoc'});
     return;
   }
