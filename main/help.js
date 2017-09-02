437e6ff6cb8d0a671837a17ceaee1cba765e3f61
shir0tetsuo <shadowsword@protonmail.com>
2017-09-02 06:41:16 -0400

    new help thing

--- main/help.js
+++ main/help.js
@@ -29,7 +29,7 @@ exports.run = (client, message, params) => {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
-      message.author.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}`, {code:'asciidoc'});
+      message.author.send(`= ${command.help.name} = \n${command.help.description}\nusage:: ${command.help.usage}\nshortcuts:: ${command.conf.aliases}`, {code:'asciidoc'});
     }
   }
 };
