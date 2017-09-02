f778baac96771920bad18c30881aca1c4fcdaff4
shir0tetsuo <shadowsword@protonmail.com>
2017-09-02 06:29:15 -0400

    major overhaul

--- main/help.js
+++ main/help.js
@@ -2,21 +2,27 @@ const settings = require('../settings.json');
 exports.run = (client, message, params) => {
   if (params[0] >= 2 || params[0] === 1 || !params[0]) {
     //const commandNames = Array.from(client.commands.keys());
-    const level = client.elevation(message)
-    const goodCommands = client.commands.filter(cmd => cmd.conf.permLevel <= level && cmd.conf.enabled !== false)
-    const commandNames = goodCommands.keyArray()
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     if (params[0] === undefined) {
       var page = 1
     } else {
       var page = params[0]
     }
+    const level = client.elevation(message)
+    // goodCommands = myCommands
+    const goodCommands = client.commands.filter(cmd => cmd.conf.permLevel <= level && cmd.conf.enabled !== false)
+    const commandNames = goodCommands.keyArray()
+    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
+    let output = `= Sabre Commands =\n\n[Page ${page}, ${settings.prefix}help <commandname> for details]\n`;
+    goodCommands.forEach( c => {
+      output += `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description} (PL${c.conf.permLevel})\n`
+    })
     let hLen = 0 + ((page*1 - 1) * 1800)
     let hMax = hLen + 1800
     console.log(hLen, hMax, level)
-    let commandMap = client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')
-    let newMap = commandMap.substring(hLen,hMax)
-    message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n[help page ${page}]\n\n${newMap}`, {code:'asciidoc'});
+    message.author.send(output.substring(hLen,hMax), {code:'asciidoc'});
+    //let commandMap = client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')
+    //let newMap = commandMap.substring(hLen,hMax)
+    //message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n[help page ${page}]\n\n${newMap}`, {code:'asciidoc'});
     return;
     } else {
     let command = params[0];
