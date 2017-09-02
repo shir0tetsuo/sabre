914151701309250f2940ee6fee6f5649f3f4a9cf
shir0tetsuo <shadowsword@protonmail.com>
2017-09-02 03:10:27 -0400

    donation command update

--- main/donate.js
+++ main/donate.js
@@ -1,7 +1,29 @@
 const settings = require('../settings.json');
 const chalk = require ('chalk');
 exports.run = (client, message, params) => {
-  message.reply(`**[D̗̬ͅo̳n̤̲̫͈̝̣a͎̥t̩͕̖e̜̱](${settings.donation})**\n.\nI'll try to be consistent.`)
+//  message.reply(`**[D̗̬ͅo̳n̤̲̫͈̝̣a͎̥t̩͕̖e̜̱](${settings.donation})**\n.\nI'll try to be consistent.`)
+  message.channel.send(`This message will self-destruct.` + {embed: {
+    color: 0x0070BA,
+    timestamp: new Date(),
+    author: {
+      name: "Hello, " + message.member.displayName,
+      icon_url: message.author.avatarURL
+    },
+    fields: [
+      {
+        name: "```Thankyou for considering a donation```",
+        value: `[D̗̬ͅo̳n̤̲̫͈̝̣a͎̥t̩͕̖e̜̱](${settings.donation})`
+      },
+      {
+        name: "```Help me build an awesome bot thing```",
+        value: `${message.member.displayName}, I couldn't of done this\nwithout the help of the discord.js community.\nCurrently, Sabre extends out to __**${client.users.size}**__ users.\nDonators may receive cool benefits in future updates.\n**v${settings.version}**`
+      }
+    ]
+  }}).then(message => {
+    setTimeout(() => {
+      message.delete()
+    }, 60000) // 1 minute
+  })
 };
 
 exports.conf = {
