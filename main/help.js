66189b84c245591560a71c00734624d05c21e107
shir0tetsuo <shadowsword@protonmail.com>
2017-09-02 05:27:18 -0400

    test

--- main/help.js
+++ main/help.js
@@ -6,7 +6,9 @@ exports.run = (client, message, params) => {
     let page = params[0]
     let hLen = 0 + ((params[0]*1 - 1) * 1800)
     console.log(hLen)
-    message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n[help page ${page}]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n').substring(hLen,(hLen+1800))}`, {code:'asciidoc'});
+    console.log(hLen*1 + 1800)
+    let commandMap = client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).substring(hLen,(hLen*1 + 1800)).join('\n')`, {code:'asciidoc'}`
+    message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n[help page ${page}]\n\n${commandMap}`, {code:'asciidoc'});
     return;
   }
   if (!params[0]) {
