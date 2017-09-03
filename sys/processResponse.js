5868864f125f8c0d846b230b4d2689b797aa5657
shir0tetsuo <shadowsword@protonmail.com>
2017-09-02 22:38:53 -0400

    new fields

--- sys/processResponse.js
+++ sys/processResponse.js
@@ -6,10 +6,23 @@ const test = [
   'Two'
 ]
 
-function random(data) {
+function Rand(data) {
+  // where data is the array
   return data[Math.floor(Math.random() * data.length)]
 }
 
+function Parse(database, case) {
+  // where database is the array and case is the string
+  if (database.some(word => case.includes(word))) {
+    return true
+  } else {
+    return false
+  }
+}
+
 module.exports = function processResponse(client, message) {
-  console.log(`${random(test)}`)
+  // make lowCase the primary content object
+  const lowCase = message.content.toLowerCase();
+  // console.log(`${Rand(test)}`)
+  // if (Parse(test, lowCase) === true)
 };
