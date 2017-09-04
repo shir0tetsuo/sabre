4050040621b9d6ea423f7b5cfd76e5e228d99fd9
shir0tetsuo <shadowsword@protonmail.com>
2017-09-04 05:24:44 -0400

    test

--- main/slots.js
+++ main/slots.js
@@ -35,11 +35,11 @@ exports.run = (client, message, params) => {
   let low_100 = Rand(slots)
   let low_010 = Rand(slots)
   let low_001 = Rand(slots)
-  var top_row = top_100 top_010 top_001
+  var top_row = top_100 + top_010 + top_001
   var top_row = top_row.split(' ')
-  var mid_row = mid_100 mid_010 mid_001
+  var mid_row = mid_100 + mid_010 + mid_001
   var mid_row = mid_row.split(' ')
-  var low_row = low_100 low_010 low_001
+  var low_row = low_100 + low_010 + low_001
   var low_row = low_row.split(' ')
   //top_row = top_row.map(function(bombs){return ++bombs;});
   top_row = top_row.map(item => item.includes('bomb'){return ++bombs;});
