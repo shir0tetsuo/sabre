429d36687dd383c997efb2c272e05f00a1aca187
shir0tetsuo <shadowsword@protonmail.com>
2017-09-04 04:13:27 -0400

    test

--- main/pin.js
+++ main/pin.js
@@ -6,14 +6,11 @@ let curren = ":tickets:"
 let chatBit = ":eye_in_speech_bubble:"
 
 exports.run = (client, message, params) => {
-  console.log(client.user)
+  //console.log(client.user)
   if (message.mentions.members.first() !== undefined) {
-    console.log(message.mentions.members.first())
+    //console.log(message.mentions.members.first())
+    console.log(message.mentions.members.first().user)
   }
-
-  /*
-  let person = message.mentions.members.first();
-  if (person === undefined) return;*/
 };
 
 exports.conf = {
