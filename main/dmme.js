9dedeca418899bfcfc8b811a4be9f719458992ae
shir0tetsuo <shadowsword@protonmail.com>
2017-09-05 23:49:31 -0400

    dmme command

---
+++ main/dmme.js
@@ -0,0 +1,36 @@
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+
+exports.run = (client, message, params) => {
+  if (!params[0]) return message.author.send(`${message.author}, some text is required with this command!`)
+  let state = `= ${message.member.displayName} is currently unavailable! =`
+  message.channel.send("```asciidoc\n" + `${state}\n\n${params} :: This message expires in 5 minutes.` + "```").then(message => {
+    setTimeout(() => {
+      message.delete()
+    }, 300000) // 5 minutes
+  })
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['afk'],
+  permLevel: 2
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: 'dmme',
+  description: 'Displays a message that states you are AFK/unavailable.',
+  usage: 'dmme [reason]'
+};
