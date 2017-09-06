710dfea26ac6fb80d0a95dfb72cee73a70420e72
shir0tetsuo <shadowsword@protonmail.com>
2017-09-05 23:33:34 -0400

    Added a template.

---
+++ sys/main_template.js
@@ -0,0 +1,30 @@
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+
+exports.run = (client, message, params) => {
+  // Everything that happens
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [''],
+  permLevel: 4
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: '',
+  description: '',
+  usage: ''
+};
