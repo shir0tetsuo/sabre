a55d48713d4b9fad592c66be1a4eca9eb08e7fbc
shir0tetsuo <shadowsword@protonmail.com>
2017-09-06 15:28:15 -0400

    modified edit system

--- main/counter.js
+++ main/counter.js
@@ -6,23 +6,25 @@ let curren = ":tickets:"
 let chatBit = ":eye_in_speech_bubble:"
 
 exports.run = (client, message, params) => {
-  message.channel.send("Calculating!")
-  var countDownDate = new Date("Jan 5, 2018 15:00:00").getTime();
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+  message.channel.send("Calculating!").then(m => {
+    var countDownDate = new Date("Jan 5, 2018 15:00:00").getTime();
+    var x = setInterval(function() {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    message.edit(`${days} d ${hours} h ${minutes} m ${seconds} s`)
-    if (distance < 0) {
-      clearInterval(x);
-      var remainder = "EXPIRED"
-    }
-  }, 1000)
+      m.edit(`${days} d ${hours} h ${minutes} m ${seconds} s`)
+      if (distance < 0) {
+        clearInterval(x);
+        var remainder = "EXPIRED"
+      }
+    }, 1000)
+  })
+
 };
 
 /*
