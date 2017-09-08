09e13cbfd0d09ad94b041fb920034d5e8875a055
shir0tetsuo <shadowsword@protonmail.com>
2017-09-08 15:48:58 -0400

    forgot to clean up mess

--- main/lock.js
+++ main/lock.js
@@ -5,12 +5,12 @@ const chalk = require ('chalk');
 let curren = ":tickets:"
 let chatBit = ":eye_in_speech_bubble:"
 function availLock(message) {
-  sql.get(`SELECT * FROM avail WHERE userId = "${mess.author.id}"`).then(row => {
+  sql.get(`SELECT * FROM avail WHERE userId = "${message.author.id}"`).then(row => {
     if (row.avail === 0) {
-    sql.run(`UPDATE avail SET avail = "1" WHERE userId = ${mess.author.id}`)
+    sql.run(`UPDATE avail SET avail = "1" WHERE userId = ${message.author.id}`)
     message.reply(`\`Account Unlocked\` Users can now mention you freely.`)
     } else {
-    sql.run(`UPDATE avail SET avail = "0" WHERE userId = ${mess.author.id}`)
+    sql.run(`UPDATE avail SET avail = "0" WHERE userId = ${message.author.id}`)
     message.reply(`\`Account Locked\` Users mentioning you will be responded with an AFK Message.`)
     }
   })
