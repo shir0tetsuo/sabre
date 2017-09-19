e1e66093e1fc723801ab063d045830707da2a11d
shir0tetsuo <shadowsword@protonmail.com>
2017-09-19 17:31:56 -0400

    forcegive init

---
+++ main/forcegive.js
@@ -0,0 +1,39 @@
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+
+exports.run = (client, message, params) => {
+  if (message.mentions.members.first() !== undefined) {
+    let persons = message.mentions.members
+    let output = '';
+    for (var i = 0; i < persons.length; i++) {
+      output += `${persons[i]}\n`
+    }
+    message.channel.send(output)
+  } else {
+    message.reply(`\`ERROR\` No user defined`)
+  }
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['fgive'],
+  permLevel: 3
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: 'forcegive',
+  description: 'Allows administrators to force ticket/byte incrementation.',
+  usage: 'forcegive [@user] [t/b] [amount]'
+};
