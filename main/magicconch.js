a88f155a2f831c50e43bd35721a7782520b3b6d6
shir0tetsuo <shadowsword@protonmail.com>
2017-09-19 20:06:27 -0400

    Simple magic conch

---
+++ main/magicconch.js
@@ -0,0 +1,48 @@
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+function Rand(data) {
+  // where data is the array
+  return data[Math.floor(Math.random() * data.length)]
+}
+const answers = [
+  'Maybe someday.',
+  'Nothing.',
+  'Neither.',
+  'I don\'t think so.',
+  'No.',
+  'Yes.',
+  'Maybe.',
+  'Try asking again.'
+]
+
+exports.run = (client, message, params) => {
+  message.channel.send({embed: {
+    color: 0xd742b2,
+    timestamp: new Date(),
+    description: `Conch says: ${Rand(answers)}\n${message.author.displayName}: ${message.params.join(' ')}`
+  }})
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['conch'],
+  permLevel: 0
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: 'magicconch',
+  description: 'Ask a Question for the Magic Conch.',
+  usage: 'magicconch [question]'
+};
