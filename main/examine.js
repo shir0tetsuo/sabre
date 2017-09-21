cd65001476dafa9e22ca497d9bd655e59274ee60
shir0tetsuo <shadowsword@protonmail.com>
2017-09-21 19:55:13 -0400

    Reverted changes because nesting sucks

--- main/examine.js
+++ main/examine.js
@@ -5,24 +5,24 @@ const chalk = require ('chalk');
 exports.run = (client, message, params) => {
   if (message.mentions.members.first() === undefined) return message.reply("No User Mentioned!")
   let person = message.mentions.members.first();
-  let output = '';
+  //let output = '';
   const millisJoined = new Date().getTime() - person.joinedAt.getTime();
   const daysJoined = Math.floor(millisJoined / 1000 / 60 / 60 / 24);
   sql.get(`SELECT * FROM scores WHERE userId = "${person.id}"`).then(row => {
-    output += `${person}: Lv ${row.level}, Tk ${row.tickets}, B ${row.chatBits}\n`
-    output += `User joined ${daysJoined} Days Ago (${message.guild.name}).\n`
-    //message.author.send(`${person}: Lv ${row.level}, Tk ${row.tickets}, B ${row.chatBits}`)
+    //output += `${person}: Lv ${row.level}, Tk ${row.tickets}, B ${row.chatBits}\n`
+    //output += `User joined ${daysJoined} Days Ago (${message.guild.name}).\n`
+    message.author.send(`${person}: Lv ${row.level}, Tk ${row.tickets}, B ${row.chatBits}\nUser joined: ${daysJoined} Days Ago (${message.guild.name})`)
   })
   sql.get(`SELECT * FROM warning WHERE userId = "${person.id}"`).then(row => {
     if (!row) {
-      //message.author.send("This user has never received a warning.")
-      output += `This user has never received a warning.\n`
+      message.author.send("This user has never received a warning.")
+      //output += `This user has never received a warning.\n`
     } else {
-      //message.author.send(`Date Warned: ${row.date} - User has ${row.times} warnings.`)
-      output += `Date Warned: ${row.date} - User has ${row.times} warnings tallied.`
+      message.author.send(`Date Warned: ${row.date} - User has ${row.times} warnings.`)
+      //output += `Date Warned: ${row.date} - User has ${row.times} warnings tallied.`
     }
   })
-  message.author.send(output)
+  //message.author.send(output)
 };
 
 exports.conf = {
