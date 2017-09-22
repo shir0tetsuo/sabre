9eca9eaeaf3ee2df086529cb2045fc6ca0adde61
shir0tetsuo <shadowsword@protonmail.com>
2017-09-22 03:24:50 -0400

    descend

--- main/devSort.js
+++ main/devSort.js
@@ -13,7 +13,7 @@ exports.run = async(client, message, params) => {
   sql.get(`SELECT * FROM scores WHERE userId = "${message.author.id}"`).then(row => {
     if (!row) return message.reply(`\`FATAL-ERROR\``)
     if (row.level >= 9000) {
-      sql.get(`SELECT * FROM scores ORDER BY level ASC LIMIT 10`).then(dat => {
+      sql.get(`SELECT * FROM scores ORDER BY level DESC LIMIT 10`).then(dat => {
         console.log(dat)
         for (i = 0; i > 10; d = dat.userId, l = dat.level, i++) {
           let output = `${d[i]} :: ${l[i]}`
