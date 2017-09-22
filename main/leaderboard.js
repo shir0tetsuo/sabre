9f18802b586f816d32d29f4824b1593f8004db75
shir0tetsuo <shadowsword@protonmail.com>
2017-09-22 04:27:59 -0400

    ldb changed

---
+++ main/leaderboard.js
@@ -0,0 +1,81 @@
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+
+function afterloop(message, output) {
+  message.channel.send(`${output}`, {code:'asciidoc'})
+}
+
+exports.run = async(client, message, params) => {
+  sql.get(`SELECT * FROM scores WHERE userId = "${message.author.id}"`).then(row => {
+    if (!row) return message.reply(`\`FATAL-ERROR\``)
+    if (row.level >= 10) {
+      let delayMil = 45000
+      let output = '';
+      if (params[0] === undefined && params[0] !== 't' && params[0] !=='b') {
+        sql.all(`SELECT * FROM scores ORDER BY level DESC LIMIT 10`).then(data => {
+          output += `__**TOP 10 :: LEVEL**__`
+          output += data.map(m => `<@${m.userId}>\`\`\`asciidoc\nLEVEL :: ${m.level}\nTICKETS :: ${m.tickets}\nBYTES :: ${m.chatBits}\`\`\``).join('\n')
+          message.channel.send(`${output}`).then(message => {
+            setTimeout(() => {
+              message.delete()
+            }, delayMil)
+          })
+        })
+      } else if (params[0] === 't') {
+        sql.all(`SELECT * FROM scores ORDER BY tickets DESC LIMIT 10`).then(data => {
+          output += `__**TOP 10 :: LEVEL**__`
+          output += data.map(m => `<@${m.userId}>\`\`\`asciidoc\nLEVEL :: ${m.level}\nTICKETS :: ${m.tickets}\nBYTES :: ${m.chatBits}\`\`\``).join('\n')
+          message.channel.send(`${output}`).then(message => {
+            setTimeout(() => {
+              message.delete()
+            }, delayMil)
+          })
+        })
+      } else if (params[0] === 'b') {
+        sql.all(`SELECT * FROM scores ORDER BY chatBits DESC LIMIT 10`).then(data => {
+          output += `__**TOP 10 :: LEVEL**__`
+          output += data.map(m => `<@${m.userId}>\`\`\`asciidoc\nLEVEL :: ${m.level}\nTICKETS :: ${m.tickets}\nBYTES :: ${m.chatBits}\`\`\``).join('\n')
+          message.channel.send(`${output}`).then(message => {
+            setTimeout(() => {
+              message.delete()
+            }, delayMil)
+          })
+        })
+      }
+
+      /* sql.get(`SELECT * FROM scores ORDER BY level DESC LIMIT 10`).then(dat => {
+        console.log(dat)
+        for (i = 0; i > 10; d = dat.userId, l = dat.level, i++) {
+          let output = `${d[i]} :: ${l[i]}`
+          message.channel.send(`${output}`, {code:'asciidoc'})
+        }
+      }) */
+    } else {
+      message.reply(`\`FATAL-ERROR\` You aren't level 10, silly!`)
+    }
+  })
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['sldb'],
+  permLevel: 2
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: 'leaderboard',
+  description: 'Displays Global Sabre Leaderboards.',
+  usage: 'leaderboard (t/b)'
+};
