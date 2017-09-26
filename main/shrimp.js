e1aec0189f0dd1ae2e0bf655e9f02ff1fb7e9a08
shir0tetsuo <shadowsword@protonmail.com>
2017-09-26 15:40:05 -0400

    Experimental modifications

--- main/shrimp.js
+++ main/shrimp.js
@@ -14,7 +14,11 @@ const cooktype = [
   'boiled',
   'broiled',
   'baked',
-  'sautee\'d'
+  'sautee\'d',
+  'toasted',
+  'roasted',
+  'chopped up',
+  'sashimi\'d'
 ]
 const returned = [
   'Shrimp-Kabobs!',
@@ -32,9 +36,111 @@ const returned = [
   'Shrimp Salad!',
   'Ordinary Shrimp!'
 ]
+const shrimpTally = [
+  '10',
+  '9',
+  '8',
+  '7',
+  '6',
+  '5',
+  '4',
+  '3',
+  '2',
+  '1'
+]
+const shrimpUnfair = [
+  '7',
+  '6',
+  '5',
+  '4',
+  '3',
+  '2',
+  '1'
+]
+
+function shrimpInit(mess) { // Convert message into mess
+  sql.get(`SELECT * FROM shrimp WHERE userId ="${mess.author.id}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO shrimp (userId, userDisplay, shrimpScore) VALUES (?, ?, ?)", [mess.author.id, mess.member.displayName, 0]);
+    }
+  }).catch(() => { // Error message generates new table instead
+    console.error;
+    console.log(chalk.redBright("The system recovered from an error (New Shrimp Entry)."))
+    sql.run("CREATE TABLE IF NOT EXISTS shrimp (userId TEXT, userDisplay TEXT, shrimpScore INTEGER)").then(() => {
+      sql.run("INSERT INTO shrimp (userId, userDisplay, shrimpScore) VALUES (?, ?, ?)", [mess.author.id, mess.member.displayName, 0]);
+    })
+  })
+}
+function scoreUpTicket(mess, xval) {
+  if (!xval) var xval = 1
+  sql.get(`SELECT * FROM scores WHERE userId = "${mess.author.id}"`).then(row => {
+    sql.run(`UPDATE scores SET tickets = ${row.tickets + xval*1} WHERE userId = ${mess.author.id}`)
+  })
+}
+function shrimpUpdate(mess, xval) {
+  if (!xval) var xval = 1
+  sql.get(`SELECT * FROM shrimp WHERE userId = "${mess.author.id}"`).then(row => {
+    sql.run(`UPDATE shrimp SET userDisplay = "${mess.member.displayName}" WHERE userId = ${mess.author.id}`)
+    sql.run(`UPDATE shrimp SET shrimpScore = "${xval}" WHERE userId = ${mess.author.id}`)
+  })
+}
 
 exports.run = (client, message, params) => {
-  message.channel.send(`${message.author} ${Rand(cooktype)} dat Shrimp. Made some ${Rand(returned)}`)
+  shrimpInit(message);
+  var judgeVariance = Math.round(Math.random() * 100)
+  var judge1 = Rand(shrimpTally)
+  var judge2 = Rand(shrimpTally)
+  var judge3 = Rand(shrimpTally)
+  var judge4 = Rand(shrimpTally)
+  if (judgeVariance >= 90) {
+    var judge5 = Rand(shrimpTally)
+  } else {
+    var judge5 = Rand(shrimpUnfair)
+  }
+  var tallyScore = judge1 + ' ' + judge2 + ' ' + judge3 + ' ' + judge4 + ' ' + judge5
+  var tallyScore = tallyScore.split(' ')
+  for (i = 0; i < tallyScore.length; i++) {
+    if (tallyScore[i] == 1) {
+      var ts[i] = "This judge is not amused. They can't finish their meal."
+    }
+    if (tallyScore[i] == 2) {
+      var ts[i] = "This judge finds that there is something fairly lacking with this shrimp."
+    }
+    if (tallyScore[i] == 3) {
+      var ts[i] = "This judge likes the preparation, but still finds it sub-par."
+    }
+    if (tallyScore[i] == 4) {
+      var ts[i] = "This judge doesn't find it the worst, but can do better."
+    }
+    if (tallyScore[i] == 5) {
+      var ts[i] = "This judge finds it within acceptable parameters."
+    }
+    if (tallyScore[i] == 6) {
+      var ts[i] = "This judge somewhat likes it, but is not the best they've had."
+    }
+    if (tallyScore[i] == 7) {
+      var ts[i] = "This judge rather enjoyed the meal."
+    }
+    if (tallyScore[i] == 8) {
+      var ts[i] = "This judge **really** enjoyed it!"
+    }
+    if (tallyScore[i] == 9) {
+      var ts[i] = "A tear was brought to their eyes before saying **'This is a meal from Heaven!'**"
+    }
+    if (tallyScore[i] == 10) {
+      var ts[i] = "The meal was **so good**, they had a heart attack and was rushed to the hospital!"
+    }
+  }
+  let output = `${message.author} ${Rand(cooktype)} dat Shrimp. Made some ${Rand(returned)}\n\n`
+  output += `__The judges have a seat at the table.__\n\n`
+  output += `Judge 1: ${ts1} They give a ${judge1}\n`
+  output += `Judge 2: ${ts2} They give a ${judge2}\n`
+  output += `Judge 3: ${ts3} They give a ${judge3}\n`
+  output += `Judge 4: ${ts4} They give a ${judge4}\n`
+  output += `Judge 5: This judge is very salty. ${ts5} They give a ${judge5}\n\n`
+  output += `Your overall score: ${judge1 + judge2 + judge3 + judge4 + judge5}`
+  message.channel.send(output)
+  //message.channel.send(`${message.author} ${Rand(cooktype)} dat Shrimp. Made some ${Rand(returned)}`)
 };
 
 /*
