d91703e26b613b401f884377fb30bc5bfdc85df2
shir0tetsuo <shadowsword@protonmail.com>
2017-09-28 14:50:39 -0400

    test

--- main/danceSwitch.js
+++ main/danceSwitch.js
@@ -61,7 +61,7 @@ exports.run = (client, message, params) => {
     }
     let danceRoom = message.guild.channels.find("name", "dancefloor")
     if (!danceRoom || danceRoom === undefined) {
-      message.guild.createChannel({name: "dancefloor"})
+      message.guild.createChannel('name', 'dancefloor')
     }
   }
   message.reply(`DANCE MODE ACTIVATED! (Check roles/channels)`)
