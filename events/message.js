0082a9b77a3573982e59ee2b38fa767e534daf0a
shir0tetsuo <shadowsword@protonmail.com>
2017-09-29 14:33:15 -0400

    test

--- events/message.js
+++ events/message.js
@@ -207,7 +207,7 @@ module.exports = message => {
       ]
       msg = message;
       console.log(msg.member.roles.first(2))
-      var roleCol = msg.member.roles[1].colorAsHex();
+      var roleCol = msg.member.roles.first(2).colorAsHex();
       if (roleCol === null || roleCol === undefined) {
         var roleCol = 0xA7A7A5
       }
