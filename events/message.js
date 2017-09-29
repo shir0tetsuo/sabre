30ad4a47fa28ee6c83749e067a1e0c13da4a25fb
shir0tetsuo <shadowsword@protonmail.com>
2017-09-29 14:31:36 -0400

    first

--- events/message.js
+++ events/message.js
@@ -206,7 +206,7 @@ module.exports = message => {
         'More quotes coming soon'
       ]
       msg = message;
-      console.log(msg.member.roles)
+      console.log(msg.member.roles.first())
       var roleCol = msg.member.roles[1].colorAsHex();
       if (roleCol === null || roleCol === undefined) {
         var roleCol = 0xA7A7A5
