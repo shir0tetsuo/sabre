3807f27745447fe6c85fb1715ffb1b4ae0d05b82
shir0tetsuo <shadowsword@protonmail.com>
2017-10-02 16:00:29 -0400

    modified cpsite

--- www/cpsite.s
+++ www/cpsite.s
@@ -1,4 +1,12 @@
 #!/bin/bash
-cp ./index.html /var/www/html-micro/sabre/
-cp ./index.html /var/www/html-ssl/sabre/
+locationOne="/var/www/html-micro/sabre/"
+locationTwo="/var/www/html-ssl/sabre/"
+cp ./index.html $locationOne
+cp ./index.html $locationTwo
+cp ./miniShrimp.js $locationOne
+cp ./miniShrimp.js $locationTwo
+cp ./sabre.css $locationOne
+cp ./sabre.css $locationTwo
+cp toggles.js $locationOne
+cp toggles.js $locationTwo
 echo Done
