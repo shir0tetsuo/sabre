4d4e5758142efee5ad4d321291b9c2b58d5c2b85
shir0tetsuo <shadowsword@protonmail.com>
2017-10-04 14:05:55 -0400

    Basic message command

---
+++ main/messg.js
@@ -0,0 +1,32 @@
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+
+exports.run = (client, message, params) => {
+  msg = message;
+  message.delete()
+  msg.channel.send(`\`${msg.member.displayName}\` ${msg.content}`)
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['m'],
+  permLevel: 3
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: 'messg',
+  description: 'Allows user to speak as Sabre.',
+  usage: 'messg [message]'
+};
