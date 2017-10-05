1b7b502a014c6e8885f814ff4e15d390834754ee
shir0tetsuo <shadowsword@protonmail.com>
2017-10-05 19:07:25 -0400

    reformatted and fixed bugs

--- main/grind.js
+++ main/grind.js
@@ -23,7 +23,7 @@ exports.run = (client, message, params) => {
     setTimeout(() => {
       noGrind.delete(message.author.id)
     }, 30000)
-    message.channel.send(`The level grind is real!\nType \`grind\` to add some stuff.`)
+    message.channel.send(`The level grind is real!\nType \`grind\` to obtain prize.`)
     .then(() => {
       message.channel.awaitMessages(response => response.content === 'grind', {
         max: 1,
@@ -33,9 +33,9 @@ exports.run = (client, message, params) => {
       .then((collected) => {
         sql.get(`SELECT * FROM scores WHERE userId = "${message.author.id}"`).then(row => {
           var grinded = row.level*750
-          if (grinded === 0) var grind = 150
+          if (grinded === 0) var grinded = 150
           const collectedU = collected.first().author
-          message.channel.send(`${collectedU} collected dat grind! There was ${grinded}${curren}`)
+          message.channel.send(`${collectedU} collected dat grind! Got ${grinded}${curren}`)
           setTimeout(() => {
             sql.get(`SELECT * FROM scores WHERE userId = "${collectedU.id}"`).then(cur => {
               sql.run(`UPDATE scores SET tickets = "${cur.tickets + grinded*1}" WHERE userId = "${collectedU.id}"`)
@@ -46,8 +46,14 @@ exports.run = (client, message, params) => {
       .catch(() => {
         message.channel.send(`Too late, the oppertunity passed.`)
       })
-  })
+    })
+
+  }
+
 };
+
+
+
 
 /*
 enabled, guildOnly, aliases, permission level
