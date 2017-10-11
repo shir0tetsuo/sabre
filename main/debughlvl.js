6b0ea2b3b8dc6718ff038793912ba373d338b781
shir0tetsuo <shadowsword@protonmail.com>
2017-10-11 06:02:19 -0400

    debuglvl

---
+++ main/debughlvl.js
@@ -0,0 +1,30 @@
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+
+exports.run = (client, message, params) => {
+  sql.run(`DELETE FROM hyperlevels WHERE userId = "${message.author.id}"`)
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['debhlvl'],
+  permLevel: 4
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: 'debughlvl',
+  description: 'Deletes bugged entries.',
+  usage: 'debughlvl\nWarning :: This deletes extra SQL entries for your user.'
+};
