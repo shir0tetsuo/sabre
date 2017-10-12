dd66f5bc6095d6e7de05db64ab4ee494a994eb8f
shir0tetsuo <shadowsword@protonmail.com>
2017-10-11 21:49:01 -0400

    new costume skeleton init

--- main/costume.js
+++ main/costume.js
@@ -5,6 +5,10 @@ const chalk = require ('chalk');
 let curren = ":tickets:"
 let chatBit = ":eye_in_speech_bubble:"
 
+function costumeTable(message, uid) {
+
+}
+
 exports.run = (client, message, params) => {
   var now = new Date();
     var milli = now.getMilliseconds(),
@@ -16,7 +20,26 @@ exports.run = (client, message, params) => {
     yr = now.getFullYear();
   if (mo == 9) {
     // RUN
+    if (params[0] === 'new' && params[1] === 'desc') {
 
+    } else if (params[0] === 'revert' || params[0] === 'set') {
+
+    } else if (params[0] === 'new' && params[1] === 'nick') {
+
+    } else if (params[0] === 'new' && params[1] === 'avatar') {
+
+    } else if (params[0] === 'view') {
+
+    } else {
+      // Print existing data, check if all variables are set
+      sql.get(`SELECT * FROM costume WHERE userId = "${message.author.id}"`).then(c => {
+        if (!c) {
+          return message.reply(`\`ERROR\` Did you run the configurations first?`)
+        }
+        let uid = message.author.id
+        let origin
+      })
+    }
   } else {
     message.reply(`Halloween's over, my guy`)
   }
@@ -39,5 +62,5 @@ name is also the command alias
 exports.help = {
   name: 'costume',
   description: 'Create and display Halloween Costumes.',
-  usage: 'costume\nNew Costume :: costume new [desc (description) / nick (nickname) / avatar (avatar URL)]'
+  usage: 'costume\nNew Costume :: costume new [desc (description) / nick (nickname) / avatar (avatar URL)]\nSee Costume :: costume view [@user]\nSet/Reset Nickname :: costume [revert/set]'
 };
