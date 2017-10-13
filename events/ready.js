68c347555d0b969a4023a66067c43e3e0db800e4
shir0tetsuo <shadowsword@protonmail.com>
2017-10-13 19:09:20 -0400

    reverted in case this is the cause

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const settings = require('../settings.json')
 module.exports = client => {
-  client.user.setPresence({ game: { name: `v${settings.version} (${settings.prefix}help)`, type: 0}})
-  client.user.setStatus("idle") // online/offline/dnd/invisible/idle
+  client.user.setPresence({ game: { name: `With ${client.guilds.size} Servers. v${settings.version}`, type: 0}})
+  client.user.setStatus("dnd") // online/offline/dnd/invisible/idle
   console.log(chalk.bgGreen.black('System Ready\nSystem Ready'));
 };
