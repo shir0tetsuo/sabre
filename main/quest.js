de7d69c5c2557d53edc90a29614d851229058bac
shir0tetsuo <shadowsword@protonmail.com>
2017-10-13 22:15:35 -0400

    staging quest

--- main/quest.js
+++ main/quest.js
@@ -6,6 +6,24 @@ const chalk = require ('chalk');
 let curren = ":tickets:"
 let chatBit = ":eye_in_speech_bubble:"
 
+const msgArray = [
+  //  00  : New Quest
+  'Area 1: Dark Forest'
+]
+
+function Rand(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+function loDam() {
+  return Math.floor(Math.random() * (15 - 1) - 1)
+}
+function medDam() {
+  return Math.floor(Math.random() * (33 - 16) - 16)
+}
+function hiDam() {
+  return Math.floor(Math.random() * (50 - 33) - 33)
+}
+
 function scoreUpTicket(mess, xval) {
   if (!xval) var xval = 1
   setTimeout(() => {
@@ -90,14 +108,86 @@ function updateChapterMicro(m, mID) {
     })
   }, 2000)
 }
+function updateUserHP(m, hmod) {
+  if (!hmod) var hmod = 1;
+  setTimeout(() => {
+    sql.get(`SELECT * FROM qchapter WHERE userId = "${m.author.id}"`).then(q => {
+      sql.run(`UPDATE qchapter SET hp = "${q.hp*1 + hmod*1}" WHERE userId = "${m.author.id}"`)
+    })
+  }, 2000)
+}
 
-function questFloor(m, q) {
+function hpGuage(hp) {
+  var icoA = ':green_heart:',
+    icoB = ':yellow_heart:',
+    icoC = ':heart:',
+    pfix = `\`HP: ${hp}\` `;
+  // A = high HP, B = med HP, C = low HP
+  if (hp >= 90) {
+    return `${pfix}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}`
+  }
+  if (hp >= 80) {
+    return `${pfix}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}`
+  }
+  if (hp >= 70) {
+    return `${pfix}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}`
+  }
+  if (hp >= 60) {
+    return `${pfix}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}${icoA}`
+  }
+  if (hp >= 50) {
+    return `${pfix}${icoB}${icoB}${icoB}${icoB}${icoB}${icoB}`
+  }
+  if (hp >= 40) {
+    return `${pfix}${icoB}${icoB}${icoB}${icoB}${icoB}`
+  }
+  if (hp >= 30) {
+    return `${pfix}${icoB}${icoB}${icoB}${icoB}`
+  }
+  if (hp >= 20) {
+    return `${pfix}${icoB}${icoB}${icoB}`
+  }
+  if (hp >= 10) {
+    return `${pfix}${icoC}${icoC}`
+  }
+  if (hp >= 1) {
+    return `${pfix}${icoC}`
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// This is where the magic happens.
+/*
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+if (chapter === 1)
+  - Chapter Area
+  - Chapter Encounter Math
+
+*/
+function questFloor(m, q, params) {
+  let content = '';
   var cno = q.chap,
     cmi = q.micro,
     cbs = q.battleState,
     chp = q.hp,
+    uin = params,
     message = m,
     uid = m.author.id;
+  if (cno == 1 && cmi == 0) {
+    content += `${hpGuage(chp)}\n`
+    content += `You arrive in the Dark Forest.\n`
+    content += `\`More to come\``
+    m.reply(`${msgArray[0]}\n${content}`)
+    updateChapterMicro(m, 1)
+  }
+  if (cno == 1 && cmi == 1) {
+    content += `${hpGuage(chp)}\n`
+    content += `You are in the Dark Forest.\n`
+    content += `\`More to come\``
+    m.reply(`${msgArray[0]}\n${content}`)
+  }
 }
 
 exports.run = (client, message, params) => {
@@ -122,12 +212,15 @@ exports.run = (client, message, params) => {
         if (cmi >= 1) {
           // NOTE: Continue quest
           //////////////////////////////////////////////////////////////////////
+          questFloor(message, q, params);
           ///////////////////////////////////////////////////////////////////EOF
         } else {
           if (hl.spaceA*1 >= 1) {
             // NOTE: Incomplete! Add deduction for key and generate quest data.
             ////////////////////////////////////////////////////////////////////
             return message.reply(`1 :key2: was spent.`)
+            questFloor(message, q, params);
+            // deduct here
             /////////////////////////////////////////////////////////////////EOF
           } else {
             return message.reply(`\`ERROR\` You don't have a quest key.`)
