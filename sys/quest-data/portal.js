721840bcb127b60fcf2ec8e1f1a1e6f8429033fb
shir0tetsuo <shadowsword@protonmail.com>
2017-10-16 17:43:15 -0400

    function layout test

--- sys/quest-data/portal.js
+++ sys/quest-data/portal.js
@@ -1,4 +1,16 @@
+function runPortal(message, h, baseHP, boss, state) {
+  if (state == 1) {
+    message.reply(`Hello, World! This is the other side of the portal.`)
+  }
+
+  state = 'EOF'
+
+  if (state !== 'EOF') {
+    runPortal(message, h, baseHP, boss, state)
+  }
+}
+
 module.exports = (message, h, baseHP, boss) => {
   // Experimental
-
+  runPortal(message, h, baseHP, boss, 1)
 }
