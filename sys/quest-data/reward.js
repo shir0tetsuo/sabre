68987bbc25a3182de7a5121e690476a9fbe2f351
shir0tetsuo <shadowsword@protonmail.com>
2017-10-16 06:19:42 -0400

    monitor reward print

--- sys/quest-data/reward.js
+++ sys/quest-data/reward.js
@@ -4,6 +4,7 @@ function generateReward(message, h) {
   var rewardPrint = Math.round(Math.random())
   // Level 1 Winnings
   if (h.hlvl >= 1) {
+    console.log(rewardPrint)
     if (rewardPrint <= 0.33) {
       var rTk = Math.round(Math.random() * (100000 - 1000 * h.hlvl) + 1000 * h.hlvl)
       var rewObject = `${rTk} Sabre Tickets`
@@ -17,6 +18,7 @@ function generateReward(message, h) {
       Transaction(message, 'dtk', 1)
     } else if (rewardPrint <= 1) {
       var rewObject = `Nothing.`
+      console.log(rewardPrint)
     }
   } else {
     var rewObject = "nothing."
