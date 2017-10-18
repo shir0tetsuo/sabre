f4482e46bceb34dc1db52d97365f212fbab1c8ba
shir0tetsuo <shadowsword@protonmail.com>
2017-10-17 22:14:49 -0400

    moderator shield cmd

---
+++ main/shields.js
@@ -0,0 +1,35 @@
+// ctrl-p = find a file
+const sql = require("sqlite");
+sql.open("../score.sqlite");
+const settings = require('../settings.json');
+const chalk = require ('chalk');
+let curren = ":tickets:"
+let chatBit = ":eye_in_speech_bubble:"
+
+exports.run = (client, message, params) => {
+  if (params[2] !== undefined && params[3] === undefined) {
+    var link = `https://img.shields.io/badge/${params[0]}-${params[1]}-${params[2]}.png`
+  } else {
+    message.reply(`\`ERROR!\` Invalid Format!`)
+  }
+};
+
+/*
+enabled, guildOnly, aliases, permission level
+*/
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['shl'],
+  permLevel: 2
+};
+
+/*
+name, desc., usage
+name is also the command alias
+*/
+exports.help = {
+  name: 'shields',
+  description: 'Make a shield thing.',
+  usage: 'shields [something%20here] [something%20there] [HTMLColor(#000000)]'
+};
