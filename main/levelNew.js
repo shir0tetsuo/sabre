8f7064160a711808167f5496393075d5866cdb32
shir0tetsuo <shadowsword@protonmail.com>
2017-10-18 00:48:11 -0400

    trying something weird

--- main/levelNew.js
+++ main/levelNew.js
@@ -12,74 +12,7 @@ let HL = ":radioactive:"
 let QKEY = ":key2:"
 let HDTK = ":pound:"
 
-exports.run = (client, message, params) => {
-  if (!uid || uid === undefined) var uid = message.author; // therefor goes by message
-  if (message.member === null) return;
-  const permlvl = client.elevation(message)
-  if (permlvl === 0) {
-    var barCol = 0x36786A
-  } else if (permlvl === 1) {
-    var barCol = 0x366394
-  } else if (permlvl === 2) {
-    var barCol = 0x802D32
-  } else if (permlvl === 3) {
-    var barCol = 0x992D22
-  } else if (permlvl === 4) {
-    var barCol = 0x980098
-  }
-  let mess = message
-  let keep = message
-
-  //var row = Tran(message, 'readL', null, message.author, params)
-
-  //var hl = Tran(message, 'readH', null, message.author, params)
-
-  sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
-    if (!r) {
-      console.log(chalk.redBright("RECOVERY =>"), chalk.yellowBright(`Table Creation in Read Mode.`))
-      sql.run(`INSERT INTO scores (userId, tickets, level, chatBits) VALUES (?, ?, ?, ?)`, [uid.id, 0, 0, 1]).then(() => {
-        sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
-          var row = r;
-        })
-      })
-    } else {
-      var row = r;
-    }
-  }).catch(() => {
-    console.error;
-    console.log(chalk.redBright("RECOVERY =>"), chalk.greenBright(`Database Creation in Read Mode.`))
-    sql.run(`CREATE TABLE IF NOT EXISTS scores (userId TEXT, tickets INTEGER, level INTEGER, chatBits INTEGER)`).then(() => {
-      sql.run(`INSERT INTO scores (userId, tickets, level, chatBits)`, [uid.id, 0, 0, 1]);
-    }).then(() => {
-      sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
-        var row = r;
-      })
-    })
-  })
-
-  sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(r => {
-    if (!r) {
-      console.log(chalk.redBright("RECOVERY =>"), chalk.yellowBright(`Table Creation in Read Mode.`))
-      sql.run(`INSERT INTO hyperlevels (userId, hlvl, spaceA, spaceB) VALUES (?, ?, ?, ?)`, [uid.id, 0, 0, 0]).then(() => {
-        sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(r => {
-          var hl = r;
-        })
-      })
-    } else {
-      var hl = r;
-    }
-  }).catch(() => {
-    console.error;
-    console.log(chalk.redBright("RECOVERY =>"), chalk.greenBright(`Database Creation in Read Mode.`))
-    sql.run(`CREATE TABLE IF NOT EXISTS hyperlevels (userId TEXT, hlvl INTEGER, spaceA TEXT, spaceB TEXT)`).then(() => {
-      sql.run(`INSERT INTO hyperlevels (userId, hlvl, spaceA, spaceB)`, [uid.id, 0, 0, 0]);
-    }).then(() => {
-      sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(r => {
-        var hl = r;
-      })
-    })
-  })
-    console.log(row, hl)
+function runMessage(row, hl, mess, barCol) {
   mess.reply("Calculating!").then(m => m.edit({embed: {
     color: barCol,
     timestamp: new Date(),
@@ -129,6 +62,82 @@ exports.run = (client, message, params) => {
       }
     ]
   }}))
+}
+
+function handleHL(r, mess, barCol) {
+  var uid = mess.author;
+  sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(h => {
+    if (!h) {
+      console.log(chalk.redBright("RECOVERY =>"), chalk.yellowBright(`Table Creation in Read Mode.`))
+      sql.run(`INSERT INTO hyperlevels (userId, hlvl, spaceA, spaceB) VALUES (?, ?, ?, ?)`, [uid.id, 0, 0, 0]).then(() => {
+        sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(h => {
+          runMessage(r, h, mess, barCol)
+        })
+      })
+    } else {
+      runMessage(r, h, mess, barCol)
+
+    }
+  }).catch(() => {
+    console.error;
+    console.log(chalk.redBright("RECOVERY =>"), chalk.greenBright(`Database Creation in Read Mode.`))
+    sql.run(`CREATE TABLE IF NOT EXISTS hyperlevels (userId TEXT, hlvl INTEGER, spaceA TEXT, spaceB TEXT)`).then(() => {
+      sql.run(`INSERT INTO hyperlevels (userId, hlvl, spaceA, spaceB)`, [uid.id, 0, 0, 0]);
+    }).then(() => {
+      sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(h => {
+        runMessage(r, h, mess, barCol)
+
+      })
+    })
+  })
+}
+
+exports.run = (client, message, params) => {
+  if (!uid || uid === undefined) var uid = message.author; // therefor goes by message
+  if (message.member === null) return;
+  const permlvl = client.elevation(message)
+  if (permlvl === 0) {
+    var barCol = 0x36786A
+  } else if (permlvl === 1) {
+    var barCol = 0x366394
+  } else if (permlvl === 2) {
+    var barCol = 0x802D32
+  } else if (permlvl === 3) {
+    var barCol = 0x992D22
+  } else if (permlvl === 4) {
+    var barCol = 0x980098
+  }
+  let mess = message
+  let keep = message
+
+  //var row = Tran(message, 'readL', null, message.author, params)
+
+  //var hl = Tran(message, 'readH', null, message.author, params)
+
+  sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
+    if (!r) {
+      console.log(chalk.redBright("RECOVERY =>"), chalk.yellowBright(`Table Creation in Read Mode.`))
+      sql.run(`INSERT INTO scores (userId, tickets, level, chatBits) VALUES (?, ?, ?, ?)`, [uid.id, 0, 0, 1]).then(() => {
+        sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
+          handleHL(r, mess, barCol)
+        })
+      })
+    } else {
+      handleHL(r, mess, barCol)
+    }
+  }).catch(() => {
+    console.error;
+    console.log(chalk.redBright("RECOVERY =>"), chalk.greenBright(`Database Creation in Read Mode.`))
+    sql.run(`CREATE TABLE IF NOT EXISTS scores (userId TEXT, tickets INTEGER, level INTEGER, chatBits INTEGER)`).then(() => {
+      sql.run(`INSERT INTO scores (userId, tickets, level, chatBits)`, [uid.id, 0, 0, 1]);
+    }).then(() => {
+      sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
+        handleHL(r, mess, barCol)
+      })
+    })
+  })
+
+
 };
 
 exports.conf = {
