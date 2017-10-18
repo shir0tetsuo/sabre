c23884ab6c66fe71f716c8078c92ece4f3751927
shir0tetsuo <shadowsword@protonmail.com>
2017-10-18 00:51:16 -0400

    new level command

--- main/level.js
+++ main/level.js
@@ -2,10 +2,98 @@ const sql = require("sqlite");
 sql.open("../score.sqlite");
 const settings = require('../settings.json');
 const chalk = require ('chalk');
-let curren = ":tickets:"
-let chatBit = ":eye_in_speech_bubble:"
+const Tran = require('../sys/TRANSACTIONS.js')
+let readL = "readL"
+let readH = "readH"
+let TK = ":tickets:"
+let CB = ":eye_in_speech_bubble:"
+let LV = ":small_orange_diamond:"
+let HL = ":radioactive:"
+let QKEY = ":key2:"
+let HDTK = ":pound:"
+
+function runMessage(row, hl, mess, barCol) {
+  mess.reply("Calculating!").then(m => m.edit({embed: {
+    color: barCol,
+    timestamp: new Date(),
+    description: `Sabre Levels; v${settings.version}`,
+  /*  footer: {
+      icon_url: client.user.avatarURL,
+      text: client.user.username
+    },*/ // Footer was removed: promise rejection from client
+    author: {
+      name: mess.member.displayName,
+      icon_url: mess.author.avatarURL
+    },
+    fields: [
+      {
+        name: `${LV}: Level`,
+        value: "```" + row.level + "```",
+        inline: true
+      },
+      {
+        name: `${TK}: Tickets`,
+        value: "```" + row.tickets + "```",
+        inline: true
+      },
+      {
+        name: `${CB}: Bytes`,
+        value: "```" + row.chatBits + "```",
+        inline: true
+      },
+      {
+        name: `${HL}: Hyperlevel`,
+        value: `\`\`\`diff\n+ ${hl.hlvl}\`\`\``,
+        inline: true
+      },
+      {
+        name: `${QKEY}: Quest Keys`,
+        value: `\`\`\`diff\n+ ${hl.spaceA}\`\`\``,
+        inline: true
+      },
+      {
+        name: `${HDTK}: Dark Tickets`,
+        value: `\`\`\`diff\n+ ${hl.spaceB}\`\`\``,
+        inline: true
+      },
+      {
+        name: "Ranking for: " + mess.author.tag + "\u200b",
+        value: `System returned message in ${m.createdTimestamp - mess.createdTimestamp}ms.`
+      }
+    ]
+  }}))
+}
+
+function handleHL(r, mess, barCol) {
+  var uid = mess.author;
+  sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(h => {
+    if (!h) {
+      console.log(chalk.redBright("RECOVERY =>"), chalk.yellowBright(`Table Creation in Read Mode.`))
+      sql.run(`INSERT INTO hyperlevels (userId, hlvl, spaceA, spaceB) VALUES (?, ?, ?, ?)`, [uid.id, 0, 0, 0]).then(() => {
+        sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(h => {
+          runMessage(r, h, mess, barCol)
+        })
+      })
+    } else {
+      runMessage(r, h, mess, barCol)
+
+    }
+  }).catch(() => {
+    console.error;
+    console.log(chalk.redBright("RECOVERY =>"), chalk.greenBright(`Database Creation in Read Mode.`))
+    sql.run(`CREATE TABLE IF NOT EXISTS hyperlevels (userId TEXT, hlvl INTEGER, spaceA TEXT, spaceB TEXT)`).then(() => {
+      sql.run(`INSERT INTO hyperlevels (userId, hlvl, spaceA, spaceB)`, [uid.id, 0, 0, 0]);
+    }).then(() => {
+      sql.get(`SELECT * FROM hyperlevels WHERE userId = "${uid.id}"`).then(h => {
+        runMessage(r, h, mess, barCol)
+
+      })
+    })
+  })
+}
 
 exports.run = (client, message, params) => {
+  if (!uid || uid === undefined) var uid = message.author; // therefor goes by message
   if (message.member === null) return;
   const permlvl = client.elevation(message)
   if (permlvl === 0) {
@@ -17,93 +105,39 @@ exports.run = (client, message, params) => {
   } else if (permlvl === 3) {
     var barCol = 0x992D22
   } else if (permlvl === 4) {
-    var barCol = 0x31BF61
+    var barCol = 0x980098
   }
   let mess = message
   let keep = message
-  sql.get(`SELECT * FROM scores WHERE userId = "${mess.author.id}"`).then(row => {
-    if (!row) return mess.reply("Your current level is 0.");
-    //console.log(mess.author.id, row.level, row.tickets, row.chatBits)
-    mess.reply("Calculating!").then(m => m.edit({embed: {
-      color: barCol,
-      timestamp: new Date(),
-      description: `Sabre Levels; v${settings.version}`,
-    /*  footer: {
-        icon_url: client.user.avatarURL,
-        text: client.user.username
-      },*/ // Footer was removed: promise rejection from client
-      author: {
-        name: mess.member.displayName,
-        icon_url: mess.author.avatarURL
-      },
-      fields: [
-        {
-          name: `:small_orange_diamond:: Level`,
-          value: "```" + row.level + "```",
-          inline: true
-        },
-        {
-          name: `${curren}: Tickets`,
-          value: "```" + row.tickets + "```",
-          inline: true
-        },
-        {
-          name: `${chatBit}: Bytes`,
-          value: "```" + row.chatBits + "```",
-          inline: true
-        },
-        {
-          name: "Ranking for: " + mess.author.tag + "\u200b",
-          value: `System returned message in ${m.createdTimestamp - mess.createdTimestamp}ms.`
-        }
-      ]
-    }})).then(m => {
-      var jobs = [
-        'csd','sbi','dsi','ddsi','bmi','ang','ss'
-      ]
-      if (jobs.some(word => keep.member.displayName.toLowerCase().includes(word))) {
-        m.react("😎")
-      }
-    }) // do something here to make special things have output +=
-    let king = ":warning:"
-    const sReply = [
-      'You\'re an awesome fella.',
-      'Thanks for your support!',
-      'I appreciate it.'
-    ]
-    if (mess.author.id === settings.ownerid) {
-      mess.reply("is the Main Developer!")
-    }
-    if (mess.author.id === settings.danid || mess.member.roles.find('name', 'Sabre Donator')) {
-      mess.channel.send({embed: {
-        color: 0x844F9B,
-        author: {
-          name: mess.member.displayName,
-          icon_color: mess.author.avatarURL
-        },
-        fields: [
-          {
-            name: "\u200b",
-            value: `**Donator:**\n${sReply[Math.floor(Math.random() * sReply.length)]}`
-          }
-        ]
-      }}).then(message => {
-        message.react("⭐");
-        message.react("🔶");
-        message.react("👍");
-        let randomChance = Math.floor(Math.random() * 100)
-        if (randomChance <= 33) {
-          message.react("🎟");
-          sql.get(`SELECT * FROM scores WHERE userId = "${mess.author.id}"`).then(row => {
-            let randomTickets = Math.floor(row.tickets/19 + 20)
-            sql.run(`UPDATE scores SET tickets = ${row.tickets + randomTickets*1} WHERE userId = "${mess.author.id}"`)
-          })
-        }
+
+  //var row = Tran(message, 'readL', null, message.author, params)
+
+  //var hl = Tran(message, 'readH', null, message.author, params)
+
+  sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
+    if (!r) {
+      console.log(chalk.redBright("RECOVERY =>"), chalk.yellowBright(`Table Creation in Read Mode.`))
+      sql.run(`INSERT INTO scores (userId, tickets, level, chatBits) VALUES (?, ?, ?, ?)`, [uid.id, 0, 0, 1]).then(() => {
+        sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
+          handleHL(r, mess, barCol)
+        })
       })
+    } else {
+      handleHL(r, mess, barCol)
     }
-  }).catch(function() {
-    console.log(chalk.redBright("Bug at levels"))
+  }).catch(() => {
+    console.error;
+    console.log(chalk.redBright("RECOVERY =>"), chalk.greenBright(`Database Creation in Read Mode.`))
+    sql.run(`CREATE TABLE IF NOT EXISTS scores (userId TEXT, tickets INTEGER, level INTEGER, chatBits INTEGER)`).then(() => {
+      sql.run(`INSERT INTO scores (userId, tickets, level, chatBits)`, [uid.id, 0, 0, 1]);
+    }).then(() => {
+      sql.get(`SELECT * FROM scores WHERE userId = "${uid.id}"`).then(r => {
+        handleHL(r, mess, barCol)
+      })
+    })
   })
+
+
 };
 
 exports.conf = {
@@ -115,6 +149,6 @@ exports.conf = {
 
 exports.help = {
   name: 'level',
-  description: 'Displays a user\'s Sabre Level.',
+  description: 'Displays a user\'s Sabrelevel Data and Hyperlevel Data.',
   usage: 'level'
 };
