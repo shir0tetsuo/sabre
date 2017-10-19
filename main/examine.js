9b371e4267eea55abf94473979d1c15cd7fcca58
shir0tetsuo <shadowsword@protonmail.com>
2017-10-19 18:19:58 -0400

    reverted

--- main/examine.js
+++ main/examine.js
@@ -4,7 +4,6 @@ const settings = require('../settings.json');
 const chalk = require ('chalk');
 exports.run = (client, message, params) => {
   if (message.mentions.members.first() === undefined) return message.reply("No User Mentioned!")
-  const string = '';
   let person = message.mentions.members.first();
   //let output = '';
   const millisJoined = new Date().getTime() - person.joinedAt.getTime();
@@ -12,25 +11,25 @@ exports.run = (client, message, params) => {
   sql.get(`SELECT * FROM scores WHERE userId = "${person.id}"`).then(row => {
     //output += `${person}: Lv ${row.level}, Tk ${row.tickets}, B ${row.chatBits}\n`
     //output += `User joined ${daysJoined} Days Ago (${message.guild.name}).\n`
-    string += `${person}: Lv ${row.level}, Tk ${row.tickets}, B ${row.chatBits}\nUser joined: ${daysJoined} Days Ago (${message.guild.name})\n`
+    message.author.send(`${person}: Lv ${row.level}, Tk ${row.tickets}, B ${row.chatBits}\nUser joined: ${daysJoined} Days Ago (${message.guild.name})`)
   })
   sql.get(`SELECT * FROM hyperlevels WHERE userId = "${person.id}"`).then(row => {
     if (!row) {
-      string += `This user does not have a hyperlevel.\n`
+      message.author.send(`This user does not have a hyperlevel.`)
     } else {
-      string += `HLVL: ${row.hlvl}, HQKY: ${row.spaceA}, HDTK: ${row.spaceB}.\n`
+      message.author.send(`HLVL: ${row.hlvl}, HQKY: ${row.spaceA}, HDTK: ${row.spaceB}.`)
     }
   })
   sql.get(`SELECT * FROM warning WHERE userId = "${person.id}"`).then(row => {
     if (!row) {
-      string += "This user has never received a warning.\n"
+      message.author.send("This user has never received a warning.")
       //output += `This user has never received a warning.\n`
     } else {
-      string += `Date Warned: ${row.date} - User has ${row.times} warnings.\n`
+      message.author.send(`Date Warned: ${row.date} - User has ${row.times} warnings.`)
       //output += `Date Warned: ${row.date} - User has ${row.times} warnings tallied.`
     }
   })
-  message.author.send(string)
+  //message.author.send(output)
 };
 
 exports.conf = {
