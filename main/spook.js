7701954a9ed2bbc873f940253e364b66977e6796
shir0tetsuo <shadowsword@protonmail.com>
2017-11-01 18:22:10 -0400

    Increased requirements, added desc.

--- main/spook.js
+++ main/spook.js
@@ -13,21 +13,21 @@ exports.run = (client, message, params) => {
       return message.reply(`\`ERROR\` HyperLevel requirement not met`)
     }
     // Set hyperlevel requirement here (hl.hlvl >= int)
-    if (hl.spaceA*1 >= 2) {
+    if (hl.spaceA*1 >= 5) {
       var person = message.mentions.members.first();
       var RandomTk = Math.floor(Math.random() * (1500000 - 30000) + 30000)
       if (!person || person === undefined || person === null) {
         message.channel.send(`OOOOOH! WOOOOAAAAAAAH! HELLOOOO! Here's ${RandomTk} Sabre Tickets! (-2 :pound:) ${message.author}!`)
-        Tran(message, "hdtk", -2)
+        Tran(message, "hdtk", -5)
         Tran(message, "tk", RandomTk)
 
       } else {
-        Tran(message, "hdtk", 2, person)
-        Tran(message, "hdtk", -2)
-        message.channel.send(`${person} was given 2 DARK TICKETS! :pound:`)
+        Tran(message, "hdtk", 5, person)
+        Tran(message, "hdtk", -5)
+        message.channel.send(`${person} was given 5 DARK TICKETS! :pound:`)
       }
     } else {
-      return message.reply(`\`ERROR\` You need 2 Dark Tickets.`)
+      return message.reply(`\`ERROR\` You need 5 Dark Tickets.`)
     }
   })
 };
@@ -48,6 +48,6 @@ name is also the command alias
 */
 exports.help = {
   name: 'spooky',
-  description: '(HL1)',
+  description: 'Give or Spend Dark Tickets for Sabre Tickets (HDTK5)',
   usage: 'spooky [@user]'
 };
