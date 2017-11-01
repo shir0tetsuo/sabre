eb75bd74fc5f630ed4cd2889e555dd3b8d9bf421
shir0tetsuo <shadowsword@protonmail.com>
2017-11-01 18:17:45 -0400

    small update echoes v on system ready, def. status DND

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const chalk = require('chalk');
 const settings = require('../settings.json')
 module.exports = client => {
   client.user.setPresence({ game: { name: `v${settings.version} (${settings.prefix}help)`, type: 0}})
-  client.user.setStatus("invisible") // online/offline/dnd/invisible/idle
-  console.log(chalk.bgGreen.black('System Ready\nSystem Ready'));
+  client.user.setStatus("dnd") // online/offline/dnd/invisible/idle
+  console.log(chalk.bgGreen.black(`System Ready (${settings.version})\nSystem Ready (${settings.version})`));
 };
