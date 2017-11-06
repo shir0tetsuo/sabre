db375b02cbc6b89e98d5a357f84008f4549a76c0
shir0tetsuo <shadowsword@protonmail.com>
2017-11-06 14:22:21 -0500

    darkshop init test

--- main/darkshop.js
+++ main/darkshop.js
@@ -8,18 +8,22 @@ let chatBit = ":eye_in_speech_bubble:"
 
 exports.run = (client, message, params) => {
   sql.get(`SELECT * FROM hyperlevels WHERE userId = "${message.author.id}"`).then(hl => {
-    if (!hl) {
-      return message.reply(`\`ERROR\` HyperLevel requirement not met`)
+    if (!hl) return message.reply(`\`ERROR\` Cannot find hyperlevel data.`)
+    var AvailableKeys = '';
+    if (hl.spaceA*1 >= 20) {
+      AvailableKeys += `20 :key2: \`3,000,000 Sabre Tickets\``
     }
-    // Set hyperlevel requirement here (hl.hlvl >= int)
-    if (hl.hlvl !== 0) {
-      /*
-
-        Command data here
-
-      */
-    } else {
-      return message.reply(`\`ERROR\` HyperLevel isn't high enough.`)
+    if (hl.spaceA*1 >= 10) {
+      AvailableKeys += `10 :key2: \`1,250,000 Sabre Tickets\``
+    }
+    if (hl.spaceA*1 >= 5) {
+      AvailableKeys += `5 :key2: \`500,000 Sabre Tickets\``
+    }
+    if (hl.spaceA*1 >= 1) {
+      AvailableKeys += `1 :key2: \`240,000 Sabre Tickets\``
+    }
+    if (hl.spaceA*1 === 0) {
+      AvailableKeys += `You don't have any keys.`
     }
   })
 };
