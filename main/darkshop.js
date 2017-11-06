9ffda177e3e79c9ea2e19a7f380aa2b2669340b8
shir0tetsuo <shadowsword@protonmail.com>
2017-11-06 14:50:58 -0500

    minor shop update

--- main/darkshop.js
+++ main/darkshop.js
@@ -14,30 +14,30 @@ exports.run = (client, message, params) => {
     const Keys = hl.spaceA*1
     const Dark = hl.spaceB*1
     if (Keys >= 20) {
-      AvailableKeys += `20 :key2: \`3,000,000 Sabre Tickets\`\n`
+      AvailableKeys += `20 :key2: \`3,000,000 Sabre Tickets\` (20k0)\n`
     }
     if (Keys >= 10) {
-      AvailableKeys += `10 :key2: \`1,250,000 Sabre Tickets\`\n`
+      AvailableKeys += `10 :key2: \`1,250,000 Sabre Tickets\` (10k0)\n`
     }
     if (Keys >= 5) {
-      AvailableKeys += `5 :key2: \`500,000 Sabre Tickets\`\n`
-      AvailableKeys += `5 :key2: \`6 Dark Tickets\`\n`
+      AvailableKeys += `05 :key2: \`500,000 Sabre Tickets\` (5k0)\n`
+      AvailableKeys += `05 :key2: \`6 Dark Tickets\` (5k1)\n`
     }
     if (Keys >= 1) {
-      AvailableKeys += `1 :key2: \`90,000 Sabre Tickets\`\n`
+      AvailableKeys += `01 :key2: \`90,000 Sabre Tickets\` (1k0)\n`
     }
     if (Keys === 0) {
       AvailableKeys += `You don't have any keys.\n`
     }
     if (Dark >= 20) {
-      AvailableDark += `20 :pound: \`20 Quest Keys\`\n`
-      AvailableDark += `20 :pound: \`Quest Trophy\`\n`
+      AvailableDark += `20 :pound: \`20 Quest Keys\` (20t0)\n`
+      AvailableDark += `20 :pound: \`Quest Trophy\` (20t1)\n`
     }
     if (Dark >= 10) {
-      AvailableDark += `10 :pound: \`9 Quest Keys\`\n`
+      AvailableDark += `10 :pound: \`9 Quest Keys\` (10t0)\n`
     }
     if (Dark >= 5) {
-      AvailableDark += `5 :pound: \`3 Quest Keys\`\n`
+      AvailableDark += `05 :pound: \`3 Quest Keys\` (10t1)\n`
     }
     if (Dark === 0) {
       AvailableDark += `You don't have any dark tickets.\n`
