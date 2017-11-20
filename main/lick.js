7554159b2d917acd580e19a7d105fa6e014d93d1
shir0tetsuo <shadowsword@protonmail.com>
2017-11-20 14:20:56 -0500

    fixed

--- main/lick.js
+++ main/lick.js
@@ -1,16 +1,13 @@
-function magicJohnson(client, message, params){
-  let aids = Math.Floor(Math.Random() * 100);
-  aids = aids % 2;
+
+exports.run = (client, message, params) => {
+  if (message.mentions.members.first() === undefined || message.mentions.members.first() === null) return message.reply(`No mention!`);
+    let aids = Math.Floor(Math.Random() * 100);
   
   if (message.mentions.members.first() !== undefined && aids >=50) {
     message.channel.send(`Oh no! ${message.mentions.members.first().displayName} has been licked by ${message.author} and was given AIDS. You've been Magic Johnson'd!`);
   } else {
     message.channel.send(`Woah! ${message.author} just tried to lick ${message.mentions.members.first().displayName} but failed to get rid of the AIDS.`);
   }
-}
-exports.run = (client, message, params) => {
-  if (message.mentions.members.first() === undefined || message.mentions.members.first() === null) return message.reply(`No mention!`);
-  magicJohnson(client, message, params);
 };
 
 /*
