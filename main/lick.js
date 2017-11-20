5fd276580d2c7c115ae9badcf496df5c72ba53f5
shir0tetsuo <shadowsword@protonmail.com>
2017-11-20 14:27:45 -0500

    removed logging

--- main/lick.js
+++ main/lick.js
@@ -1,17 +1,12 @@
 
 exports.run = (client, message, params) => {
-  console.log(`Run`)
   if (message.mentions.members.first() === undefined || message.mentions.members.first() === null) return message.reply(`No mention!`);
     var aids = Math.floor(Math.random() * 100);
   
-  console.log(aids)
-  
   if (message.mentions.members.first() !== undefined && aids >=50) {
     message.channel.send(`Oh no! ${message.mentions.members.first().displayName} has been licked by ${message.author} and was given AIDS. You've been Magic Johnson'd!`);
-  console.log(`Done`)
   } else {
     message.channel.send(`Woah! ${message.author} just tried to lick ${message.mentions.members.first().displayName} but failed to get rid of the AIDS.`);
-  console.log(`Done`)
   }
 };
 
