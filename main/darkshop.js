8bbd4991e888983aa7769b67d0fdb7c42895cedf
root <root@localhost.localdomain>
2017-11-27 14:43:28 -0500

    test

--- main/darkshop.js
+++ main/darkshop.js
@@ -6,6 +6,10 @@ const chalk = require ('chalk');
 let curren = ":tickets:"
 let chatBit = ":eye_in_speech_bubble:"
 
+const availablePossible = [
+  '20k0', '10k0', '5k0', '5k1', '1k0', '20t0', '20t1', '10t0', '5t0'
+]
+
 exports.run = (client, message, params) => {
   sql.get(`SELECT * FROM hyperlevels WHERE userId = "${message.author.id}"`).then(hl => {
     if (!hl) return message.reply(`\`ERROR\` Cannot find hyperlevel data.`)
@@ -13,34 +17,38 @@ exports.run = (client, message, params) => {
     var AvailableDark = '';
     const Keys = hl.spaceA*1
     const Dark = hl.spaceB*1
-    if (Keys >= 20) {
-      AvailableKeys += `\`20\` :key2: \`3,000,000 Sabre Tickets\` (20k0)\n`
-    }
-    if (Keys >= 10) {
-      AvailableKeys += `\`10\` :key2: \`1,250,000 Sabre Tickets\` (10k0)\n`
-    }
-    if (Keys >= 5) {
-      AvailableKeys += `\`05\` :key2: \`500,000 Sabre Tickets\` (5k0)\n`
-      AvailableKeys += `\`05\` :key2: \`6 Dark Tickets\` (5k1)\n`
-    }
-    if (Keys >= 1) {
-      AvailableKeys += `\`01\` :key2: \`90,000 Sabre Tickets\` (1k0)\n`
-    }
-    if (Keys === 0) {
-      AvailableKeys += `You don't have any keys.\n`
-    }
-    if (Dark >= 20) {
-      AvailableDark += `\`20\` :pound: \`20 Quest Keys\` (20t0)\n`
-      AvailableDark += `\`20\` :pound: \`Quest Trophy\` (20t1)\n`
-    }
-    if (Dark >= 10) {
-      AvailableDark += `\`10\` :pound: \`9 Quest Keys\` (10t0)\n`
-    }
-    if (Dark >= 5) {
-      AvailableDark += `\`05\` :pound: \`3 Quest Keys\` (5t0)\n`
-    }
-    if (Dark === 0) {
-      AvailableDark += `You don't have any dark tickets.\n`
+    if (params[0].indexOf(availablePossible) !== -1) {
+      message.channel.send(`Success! \`Not Really!\``)
+    } else {
+      if (Keys >= 20) {
+        AvailableKeys += `\`20\` :key2: \`3,000,000 Sabre Tickets\` (20k0)\n`
+      }
+      if (Keys >= 10) {
+        AvailableKeys += `\`10\` :key2: \`1,250,000 Sabre Tickets\` (10k0)\n`
+      }
+      if (Keys >= 5) {
+        AvailableKeys += `\`05\` :key2: \`500,000 Sabre Tickets\` (5k0)\n`
+        AvailableKeys += `\`05\` :key2: \`6 Dark Tickets\` (5k1)\n`
+      }
+      if (Keys >= 1) {
+        AvailableKeys += `\`01\` :key2: \`90,000 Sabre Tickets\` (1k0)\n`
+      }
+      if (Keys === 0) {
+        AvailableKeys += `You don't have any keys.\n`
+      }
+      if (Dark >= 20) {
+        AvailableDark += `\`20\` :pound: \`20 Quest Keys\` (20t0)\n`
+        AvailableDark += `\`20\` :pound: \`Quest Trophy\` (20t1)\n`
+      }
+      if (Dark >= 10) {
+        AvailableDark += `\`10\` :pound: \`9 Quest Keys\` (10t0)\n`
+      }
+      if (Dark >= 5) {
+        AvailableDark += `\`05\` :pound: \`3 Quest Keys\` (5t0)\n`
+      }
+      if (Dark === 0) {
+        AvailableDark += `You don't have any dark tickets.\n`
+      }
     }
     message.channel.send(`${AvailableKeys}${AvailableDark}`)
     // trophies will be obtained by killing quest bosses.
