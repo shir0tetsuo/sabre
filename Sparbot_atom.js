db943d550c61f2dcf539c71fedeba22eea0583b0
root <root@localhost.localdomain>
2018-03-13 04:24:35 -0400

    Added Sustanance

--- Sparbot_atom.js
+++ Sparbot_atom.js
@@ -51,15 +51,105 @@ INTText += `:three: The ${mach} **is a master of its domain.** A slightly dumber
 INTText += `:four: The ${mach} **is like facing a Jedi Master.** Oh, and it can now use flight and illusions. Seriously, get out your lucky rabbit's foot.`
 
 function InvokeSpar(message){
+  const ActFour = (['1', '2', '3', '4'])
+  const ActFive = (['1', '2', '3', '4', '5'])
+  const ActSix = (['1', '2', '3', '4', '5', '6'])
+  const ActTen = (['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'])
+
   message.reply(`Please state a time limit between 1-10 minutes.`)
-
-  message.reply(`Please state the level of defense. ${DLText}`)
-
-  message.reply(`Please state the level of speed. ${SPDText}`)
-
-  message.reply(`Please state the level of attack. ${ATKText}`)
-
-  message.reply(`Please state the level of intelligence. ${INTText}`)
+  message.channel.awaitMessages(TL => TL.author.id === message.author.id && ActTen.some(word => TL.content.startsWith(word)), {
+    max: 1,
+    time: 60000,
+    errors: ['time'],
+  })
+  .then(FinishedTime => {
+    LimitQuery = FinishedTime.first().content;
+    if (LimitQuery === "1") {
+      var limiter = 60000
+    } else if (LimitQuery === "2") {
+      var limiter = 120000
+    } else if (LimitQuery === "3") {
+      var limiter = 180000
+    } else if (LimitQuery === "4") {
+      var limiter = 240000
+    } else if (LimitQuery === "5") {
+      var limiter = 300000
+    } else if (LimitQuery === "6") {
+      var limiter = 360000
+    } else if (LimitQuery === "7") {
+      var limiter = 420000
+    } else if (LimitQuery === "8") {
+      var limiter = 480000
+    } else if (LimitQuery === "9") {
+      var limiter = 540000
+    } else if (LimitQuery === "10") {
+      var limiter = 600000
+    } else {
+      message.reply(`\`Internal Error\``)
+      return;
+    }
+    message.reply(`Please state the level of defense. ${DLText}`)
+    message.channel.awaitMessages(DS => DS.author.id === message.author.id && ActFive.some(word => DS.content.startsWith(word)), {
+      max: 1,
+      time: 60000,
+      errors: ['time'],
+    })
+    .then(() => {
+      message.reply(`Please state the level of speed. ${SPDText}`)
+      message.channel.awaitMessages(SS => SS.author.id === message.author.id && ActFive.some(word => SS.content.startsWith(word)), {
+        max: 1,
+        time: 60000,
+        errors: ['time'],
+      })
+      .then(() => {
+        message.reply(`Please state the level of attack. ${ATKText}`)
+        message.channel.awaitMessages(AS => AS.author.id === message.author.id && ActSix.some(word => AS.content.startsWith(word)), {
+          max: 1,
+          time: 60000,
+          errors: ['time'],
+        })
+        .then(() => {
+          message.reply(`Please state the level of intelligence. ${INTText}`)
+          message.channel.awaitMessages(IS => IS.author.id === message.author.id && ActFour.some(word => IS.content.startsWith(word)), {
+            max: 1,
+            time: 60000,
+            errors: ['time'],
+          })
+          .then(() => {
+            message.reply(`**your sparring specifications have been collected!** Good luck!`)
+            setTimeout(() => {
+              message.reply(`**Expiry!** The construct and ${mach} has been dematerialized.`)
+            }, limiter)
+          })
+          .catch(() => {
+            console.error;
+            console.log(`${mach} BREAK IS`)
+            message.reply(`Reply time expired.`)
+          })
+        })
+        .catch(() => {
+          console.error;
+          console.log(`${mach} BREAK AS`)
+          message.reply(`Reply time expired.`)
+        })
+      })
+      .catch(() => {
+        console.error;
+        console.log(`${mach} BREAK SS`)
+        message.reply(`Reply time expired.`)
+      })
+    })
+    .catch(() => {
+      console.error;
+      console.log(`${mach} BREAK DS`)
+      message.reply(`Reply time expired.`)
+    })
+  })
+  .catch(() => {
+    console.error;
+    console.log(`${mach} BREAK null`)
+    message.reply(`Reply time expired.`)
+  })
 }
 
 client.login(settings.token_sparbot);
