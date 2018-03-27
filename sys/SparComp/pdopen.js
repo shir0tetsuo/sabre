b20246875d41b02447621434498f6438026ca3c1
root <root@localhost.localdomain>
2018-03-27 02:47:21 -0400

    -8.1 Near Completion

--- sys/SparComp/pdopen.js
+++ sys/SparComp/pdopen.js
@@ -81,7 +81,11 @@ function MaterialTime(message) {
     })
     .then(act => {
       var time = Math.round(act.first().content * 1) * 60000
-      message.reply(`\`Your Pocket Dimension has been opened for ${act.first().content} minutes.\``)
+      if (time * 1 === 60000) {
+        message.reply(`\`Your pocket dimension has been opened for 1 minute.`)
+      } else {
+        message.reply(`\`Your Pocket Dimension has been opened for ${act.first().content} minutes.\``)
+      }
       setTimeout(() => {
         message.reply(`\`Your Pocket Dimension has closed and you have been evicted.\``)
       }, time)
